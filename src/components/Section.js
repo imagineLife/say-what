589f10cab13bf268563f54aedcca9a4d2580e714
Eric Laursen <ericjlaursen@gmail.com>
2017-10-28 13:19:34 -0400

    includes variable return based on props

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,15 +2,29 @@ import React from 'react';
 import './Section.css';
 import Title from './Title';
 import Para from './Para';
+import BeginForm from './BeginForm';
 
 export default function Section(props) {
 
+	//If the section contains a form
+	//render that component with form
+	if(props.includeBeginForm){
 	    return (
 			<section>
 				<Title title={props.title}/>
 		        {props.img}
 		        <Para text={props.text}/>
-		        {props.form}
+		        <BeginForm />
 		    </section>
-	    );
+	    );			
+	}
+
+    return (
+		<section>
+			<Title title={props.title}/>
+	        {props.img}
+	        <Para text={props.text}/>
+	        {props.form}
+	    </section>
+    );
 }
