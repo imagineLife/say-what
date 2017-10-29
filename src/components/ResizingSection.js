313fbec3eaf3a1a63201a0175d559e8c8fe65e01
Eric Laursen <ericjlaursen@gmail.com>
2017-10-29 07:44:02 -0400

    removing nested block errors

--- src/components/ResizingSection.js
+++ src/components/ResizingSection.js
@@ -10,60 +10,57 @@ export default function ResizingSection(props) {
 	
 	//If the section contains a word-list,
 	//render that component with the word-list
-		if(props.WordList){
-		//build the looping word-list to output a list item
-			const listOWords = props.WordList.map((word, ind) =>
-				<Li key={ind} word={word} />
-			)
+	if(props.WordList){
+	//build the looping word-list to output a list item
+		const listOWords = props.WordList.map((word, ind) =>
+			<Li key={ind} word={word} />
+		)
 
-			return (
-				<section className="col-6">
-					<Title title={props.title}/>
-			        {props.img}
-			        <ul>
-			        	{listOWords}
-			        </ul>
-			    </section>
-		    );
-		}
-
-		if(props.qStats){
-{/* If the section contains the quick-stats,
-render that component with the word-list */}
-			const singleStat = Object.keys(props.qStats).map(key => {
-				return <Li key={key} word={props.qStats[key]} boldWord={key} />
-			})
-
-			return (
-				<section className="col-6">
-					<Title title={props.title}/>
-				    {props.img}
-				    <ul>
-				    	{singleStat}
-				    </ul>
-				</section>
-			);
-		}
-
-		else if(props.includeBeginForm){
-{/*If the section contains a form
-render that component with form*/}
-
-			return (
-				<section className="col-6">
-					<Title title={props.title}/>
-			        {props.img}
-			        <BeginForm />
-			    </section>
-		    );			
-		}
-
-	//plane-Jane section
-	    return (
+		return (
 			<section className="col-6">
 				<Title title={props.title}/>
 		        {props.img}
-		        <Para text={props.text}/>
+		        <ul>
+		        	{listOWords}
+		        </ul>
 		    </section>
 	    );
+	}
+
+	// If the section contains the quick-stats, render that component with the word-list
+	else if(props.qStats){
+		const singleStat = Object.keys(props.qStats).map(key => {
+			return <Li key={key} word={props.qStats[key]} boldWord={key} />
+		})
+
+		return (
+			<section className="col-6">
+				<Title title={props.title}/>
+			    {props.img}
+			    <ul>
+			    	{singleStat}
+			    </ul>
+			</section>
+		);
+	}
+
+	//If the section contains a form render that component with form
+	else if(props.includeBeginForm){ 
+		return (
+			<section className="col-6">
+				<Title title={props.title}/>
+		        {props.img}
+		        <BeginForm />
+		    </section>
+	    );			
+	}
+
+	//plane-Jane section
+	return (
+		<section className="col-6">
+			<Title title={props.title}/>
+	        {props.img}
+	        <Para text={props.text}/>
+	    </section>
+	);
 }
