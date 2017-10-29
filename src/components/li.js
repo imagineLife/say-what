2df6fb09dd2f3c99bf44bc23236f7b228dc6a988
Eric Laursen <ericjlaursen@gmail.com>
2017-10-29 07:05:43 -0400

    includes conditional output

--- src/components/li.js
+++ src/components/li.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './li.css';
 
 export default function Li(props) {
+	console.log(Object.keys(props).length);
+	let objSize = Object.keys(props).length;
+	switch(objSize) {
+		// case (props.hasOwnProperty('boldWord')) :
+		// case (objSize > 1) :
+		case (objSize = 2) :
+		    return (
+				<li><span className="boldWord">{props.boldWord} :</span> {props.word}</li>
+		    );
+			break;
 
-    return (
-		<li>{props.word}</li>
-    );
+		default:
+		    return (
+				<li>{props.word}</li>
+		    );
+	}
 }
