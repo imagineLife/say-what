1dc8ddad9942c020b491320efcab10df4b1a0486
Eric Laursen <ericjlaursen@gmail.com>
2017-10-29 12:29:24 -0400

    reconfigs sections array

--- src/SpeechPicker.js
+++ src/SpeechPicker.js
@@ -5,35 +5,33 @@ import Section from './components/Section';
 export default function SpeechPicker(props) {
 
 		const pageHeader = {
-			title: `Say What?!`,
-			text: `See data within speeches`
+			title: `Pick a Speech`,
+			text: ``
 		}
 
 		const sectionsArray =[
 			{
-				title: `Notice what they're saying`,
-				text: `SayWhay looks at a speech and its text content as a dataset, illuminating the largest words spoken, the most common words of the speech, and more!`
-			},
-			{
-				title: `Pick from a variety of Speeches`,
-				text: `Interested in the details of what Trump said? Maybe Hillary? MLK Jr? Perhaps JFK or Abraham Lincoln?
-				Get insight into the words of a variety of speeches.`,
-				img: 'IMAGE OF SPEAKER'
-			},
-			{
-				title: 'Check it out!',
-				includeBeginForm: true
+				title: `Choose from a list of options`,
+				text: ``,
+				speechPicker: true,
+				speechesArray : [
+					{
+						Orator : 'theMouth',
+						title : 'Speech Title 1'
+					},
+					{
+						Orator : 'theHeart',
+						title : 'Speech Title 2'
+					},
+					{
+						Orator : 'theSoul',
+						title : 'Speech Title 3'
+					}
+				]
 			}
 		];
-		
-		const sections = sectionsArray.map((sec,ind) => {
-			//figure out if theres an img in the sectionsArray
-			//the image will either equal itself, or set it to an empty string
-			sec.img = (sec.img)? sec.img : '';
 
-	      	//figure out if theres an wordList in the sectionsArray
-			//the wordList will either equal itself, or set it to an empty string
-			sec.WordList = (sec.WordList)? sec.WordList : '';
+		const sections = sectionsArray.map((sec,ind) => {
 	      	return <Section key={ind} {...sec}/>;
 		})
 
@@ -47,7 +45,7 @@ export default function SpeechPicker(props) {
 	        <h2>{pageHeader.text}</h2>
 	      </header>
 	      
-	      {sections} 
+	      {sections}
 
 	    </main>
     );
