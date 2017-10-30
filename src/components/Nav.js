f1def426502a31cfe84ce8e3fe0df1cc0ebc0ae5
Eric Laursen <ericjlaursen@gmail.com>
2017-10-30 06:48:06 -0400

    add static ul in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Nav.css';
+// import NavUl from './navul';
 
 export default function App() {
+	// const navItems = ['Home', 'See Some Data', 'Sign Up'];
 
     return (
-		<nav>Nav: Responsive? Sandwich in Mobile?</nav>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>See Some Data</li>
+				<li>Sign Up</li>
+			</ul>
+		</nav> 
     );
 }
