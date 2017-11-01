bd7b979d02abe145d21cfa0f7920d2f43c006e9c
Eric Laursen <ericjlaursen@gmail.com>
2017-11-01 18:54:40 -0400

    converts from function to class

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,17 +7,33 @@ import BeginForm from './BeginForm';
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 
-export default function Section(props) {
-	return (
-		<section>
-			<Title title={props.title}/>
-	        <Para text={props.text}/>
-	        {props.img}
-	        {props.speechPicker ? <Ul list={props.speechesArray} /> : ''}
-	        {props.form}
-	        {props.includeBeginForm ? <BeginForm /> : ''}
-	        {props.includesLoginForm ? <LoginForm /> : ''}
-	        {props.includesRegisterForm ? <RegisterForm /> : ''}
-	    </section>
-	);
+export default class Section extends React.Component {
+	constructor(props){
+		super(props);
+		this.title = props.title;
+	};
+
+	//add key/val to state PRE-Component-loading
+	componentWillMount(){
+		this.setState({loginOrRegister : 'login'});
+	}
+	
+	render(){
+		let toggleForm = () => {
+			let loginOrRegister = (this.state.loginOrRegister == 'login') ? 'register' : 'login';
+			this.title = loginOrRegister.charAt(0).toUpperCase() + loginOrRegister.slice(1);
+			this.setState({loginOrRegister});
+		}
+		return (
+			<section>
+				<Title title={this.title}/>
+		        <Para text={this.props.text}/>
+		        {this.props.img}
+		        {this.props.speechPicker ? <Ul list={this.props.speechesArray} /> : ''}
+		        {this.props.form}
+		        {this.props.includeBeginForm ? <BeginForm /> : ''}
+		        {this.state.loginOrRegister == 'login' ? <LoginForm toggleForm={toggleForm} /> : <RegisterForm toggleForm={toggleForm} />}
+		    </section>
+		)
+	};
 }
