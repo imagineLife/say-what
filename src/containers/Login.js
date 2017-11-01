a5b58559150ab153f6a52ce0431c94740a35a213
Eric Laursen <ericjlaursen@gmail.com>
2017-10-31 20:46:38 -0400

    placeholder login pages

---
+++ src/containers/Login.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Splash.css';
+import Header from '../components/Header';
+import Section from '../components/Section';
+
+export default function SpeechPicker(props) {
+
+		const pageHeader = {
+			title: `Pick a Speech`,
+			text: ``
+		}
+
+		const sectionsArray =[
+			{
+				title: `Choose from a list of options`,
+				text: ``,
+				speechPicker: true,
+				speechesArray : [
+					{
+						Orator : 'theMouth',
+						title : 'Wordy Speech Title'
+					},
+					{
+						Orator : 'theHeart',
+						title : 'Heartfelt compassionate speech title'
+					},
+					{
+						Orator : 'theSoul',
+						title : 'Deep, convicting, and moving speech title'
+					}
+				]
+			}
+		];
+
+		const sections = sectionsArray.map((sec,ind) => {
+	      	return <Section key={ind} {...sec}/>;
+		})
+
+
+
+
+    return (
+		<main role="main">
+		  <Header title={pageHeader.title}/>
+	      
+	      {sections}
+
+	    </main>
+    );
+}
