4fbb0f5a34cc857a9bcabf78e8c8971c7b242f85
Eric Laursen <ericjlaursen@gmail.com>
2017-11-03 06:49:28 -0400

    includes non-functional link to speech text

--- src/containers/SpeechData.js
+++ src/containers/SpeechData.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../float-grid.css';
 import './Splash.css';
 import ResizingSection from '../components/ResizingSection';
+import {Link} from 'react-router-dom';
 
 export default function App(props) {
 
@@ -56,6 +57,7 @@ export default function App(props) {
 	      <header role="banner">
 	        <h1>{pageHeader.title}</h1>
 	        <p>{pageHeader.text}</p>
+			<Link to={`/`}>Read the full Speech Text here</Link>
 	      </header>
 	      
 	      {sections} 
