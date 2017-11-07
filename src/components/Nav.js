e138676eb9f34e7ce7a281d5be91c298d4e1c140
Eric Laursen <ericjlaursen@gmail.com>
2017-11-07 06:17:51 -0500

    add responsiveness to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,16 +10,19 @@ export default function Nav() {
 				<li className='liWithIcon'>
 					<a href='/'>
 						<img src={process.env.PUBLIC_URL+'/imgs/home.ico'} className="icon" alt='home' width="50" height="auto"/>
+						<span className="menuLabel">Home</span>
 					</a>
 				</li>
 				<li className='liWithIcon'>
 					<a href='/speechData/alpha'>
 						<img src={process.env.PUBLIC_URL+'/imgs/search.ico'} className="icon" alt='View A Speech' />
+						<span className="menuLabel">View A Speech</span>
 					</a>
 				</li>
 				<li className='liWithIcon'>
 					<a href="/login">
 						<img src={process.env.PUBLIC_URL+'/imgs/user.ico'} className="icon" alt='Sign Up' />
+						<span className="menuLabel">Sign Up</span>
 					</a>
 				</li>
 			</ul>
