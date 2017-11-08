71620ec6bbaabd74ef238b4081ca2166a4308e27
Eric Laursen <ericjlaursen@gmail.com>
2017-11-08 06:32:42 -0500

    includes multi-select

--- src/components/d3/horzBarChar.js
+++ src/components/d3/horzBarChar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './horzBarChar.css';
 import * as d3 from 'd3';
+import 'd3-selection-multi';
 
 export default function HorizontalBarChart(props) {
-	//EPIC 
+
 	let data = [];
 
 	// set the dimensions and margins of the graph
 	var margin = {top: 20, right: 20, bottom: 30, left: 40},
-	    width = 960 - margin.left - margin.right,
-	    height = 500 - margin.top - margin.bottom;
+	    width = 480 - margin.left - margin.right,
+	    height = 250 - margin.top - margin.bottom;
+	    // width = document.querySelector("div").clientWidth,
+	    // height = document.querySelector("div").clientHeight;
 
 	// set the ranges
 	var y = d3.scaleBand()
@@ -23,21 +26,25 @@ export default function HorizontalBarChart(props) {
 	// append a 'group' element to 'svg'
 	// moves the 'group' element to the top left margin
 	var svg = d3.select("body").append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
+	// var svg = d3.select(this.parentNode).append("svg")
+	    .attrs({
+	    	"width" : width + margin.left + margin.right,
+	    	"height" : height + margin.top + margin.bottom
+	    })
 	  .append("g")
 	    .attr("transform", 
 	          "translate(" + margin.left + "," + margin.top + ")");
 
 	//props.dataKey is the name of the prop in React
 	for(let k in props.dataKey){
-		console.log(k);
-		//converts props data to this-needed-format
-		// let obj = {word: k, frequency: +props.dataKey[k]};
-		// data.push(obj);
-	}
+		let actualObj = props.dataKey[k];
+		let objKey = Object.keys(actualObj);
+		let objVal = Object.values(actualObj);
 
-	console.log(data);
+		//converts props data to this-needed-format
+		let obj = {word: objKey, frequency: +objVal};
+		data.push(obj);
+	}
 
 	// Scale the range of the data in the domains
 	x.domain([0, d3.max(data, function(d){ return d.frequency; })])
@@ -49,10 +56,14 @@ export default function HorizontalBarChart(props) {
 		.data(data)
 		//all chains & callbacks reference data array 
 		.enter().append("rect")
-		  .attr("class", "bar")
-		  .attr("width", function(d) {return x(d.frequency); } )
-		  .attr("y", function(d) { return y(d.word); })
-		  .attr("height", y.bandwidth());
+		  .attrs({
+		  	"class" : "bar",
+		  	"width" : (d) => {return x(d.frequency); },
+		  	"y" : (d) => { return y(d.word); },
+		  	"height" : y.bandwidth()
+		  });
+
+
 
 	// add the x Axis
 	svg.append("g")
