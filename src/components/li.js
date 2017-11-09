a74ec536799fc08fa0ec331f6707d99e7fc8c76c
Eric Laursen <ericjlaursen@gmail.com>
2017-11-09 18:54:37 -0500

    includes class for li

--- src/components/li.js
+++ src/components/li.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './li.css';
 
 export default function Li(props) {
+	console.log('LI props =>',props);
 	let objSize = Object.keys(props).length;
 
 	switch(objSize) {
 		case 2 :
 		    return (
-				<li><span className="boldWord">{props.boldWord} : </span> {props.word}</li>
+				<li className="li"><span className="boldWord">{props.boldWord} : </span> {props.word}</li>
 			);
 
 		default :
