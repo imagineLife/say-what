77582c9c1a40bef9efac6eb391c9ad5e9c5cb9fd
Eric Laursen <ericjlaursen@gmail.com>
2017-11-09 17:34:02 -0500

    includes switch case on props

--- src/components/ResizingSection.js
+++ src/components/ResizingSection.js
@@ -9,19 +9,30 @@ import HorizontalBarChart from './d3/horzBarChar';
 import BottomSpacer from './BottomSpacer';
 
 export default function ResizingSection(props) {
-	// console.log(props);
 
 	let listOWords;
 	let singleStat;
+	let wordsBySizeList;
 
 	//WORD LIST SECTION
-	if(props.WordList){
+	if(props.WordList || props.bigWords){
 
-	// build a looping word-list,
-	// outputting a single Li compoent for each word in the wordList
-		listOWords = props.WordList.map((word, ind) =>
-			<Li key={ind} word={word} />
-		)
+		switch(props){
+			case (props.WordList) :
+				// build a looping word-list,
+				// outputting a single Li compoent for each word in the wordList
+				listOWords = props.WordList.map((word, ind) =>
+					<Li key={ind} word={word} />
+				);
+				break;
+
+			default:
+				listOWords = props.bigWords.map((word, ind) =>
+					<Li key={ind} word={word} />
+				);
+				break;
+		}
+
 	}
 
 	//QUICK-STATS SECTION
@@ -30,9 +41,18 @@ export default function ResizingSection(props) {
 			if(key !== 'Title'){
 				return <Li key={key} boldWord={key} word={props.introInfo[key]} />
 			}
+			return null;
 		})
 	}
 
+	//Words-By-Size section
+	if(props.wordsBySize){		
+		wordsBySizeList = props.wordsBySize.map(function(obj) { 
+			let keyToStr = Object.keys(obj).toString();
+			let valToStr = Object.values(obj).toString();			
+			return <Li key={keyToStr} boldWord={keyToStr} word={valToStr} />
+		});
+	}
 
 	return (
 		<section className="col-6">
@@ -42,6 +62,7 @@ export default function ResizingSection(props) {
 	        {singleStat ? singleStat : ''}
 	        {props.includeBeginForm ? <BeginForm /> : ''}
 	        <Para text={props.text}/>
+	        {props.wordsBySize ? wordsBySizeList : null}
 	        {props.includeHorizontalChart ? <HorizontalBarChart attach={this} dataKey={props.mostUsedWords}/> : null}
 	        {props.includeBottomSpace ? <BottomSpacer /> : null}
 	    </section>
