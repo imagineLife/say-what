7395c579364e5d8c53bede1da7d2ec753d1d796f
Eric Laursen <ericjlaursen@gmail.com>
2017-11-10 06:20:47 -0500

    updating stule & config of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,33 @@ import React from 'react';
 import './Header.css';
 
 export default function Header(props) {
+	console.log('props.title is =>',props.title);
+	
+	let subTitle, title;
+	let headerStyle;
+
+	if(props.imagePr){
+		headerStyle = {
+		  background: `url(${ props.imagePr }) no-repeat center center`,
+		};
+	}
+
+	
+	if(props.subTitle){		
+		let subTitle = <h3>{props.subTitle}</h3>;
+		return subTitle;
+	}
+
+	if(props.title){		
+		title = <h3>{props.title.toString()}</h3>;
+		return title;
+	}
+
 
     return (
-      <header role="banner">
-        <h1>{props.title}</h1>
-        <h3>{props.subTitle}</h3>
+      <header style={headerStyle} role="banner">
+      	{title}
+        {subTitle}
       </header>
     );
 }
