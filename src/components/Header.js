bbebd60350a7cd6cfaa24845d2fb54e2c641e0df
Eric Laursen <ericjlaursen@gmail.com>
2017-11-10 06:48:32 -0500

    simplifies output

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,14 @@ import React from 'react';
 import './Header.css';
 
 export default function Header(props) {
-	console.log('props.title is =>',props.title);
-	
-	let subTitle, title;
-	let headerStyle;
-
-	if(props.imagePr){
-		headerStyle = {
-		  background: `url(${ props.imagePr }) no-repeat center center`,
-		};
-	}
-
-	
-	if(props.subTitle){		
-		let subTitle = <h3>{props.subTitle}</h3>;
-		return subTitle;
-	}
-
-	if(props.title){		
-		title = <h3>{props.title.toString()}</h3>;
-		return title;
-	}
-
+	let sectionStyle = {
+	  background: `url(${ props.imagePr }) no-repeat center center`,
+	};
 
     return (
-      <header style={headerStyle} role="banner">
-      	{title}
-        {subTitle}
+      <header style={sectionStyle} role="banner">
+        <h1 className="titleH1">{props.title}</h1>
+        <h3 className="subTitle">{props.subTitle}</h3>
       </header>
     );
 }
