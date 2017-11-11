28bae3b6f13b58ae47d03afd39a1183f51f9579c
Eric Laursen <ericjlaursen@gmail.com>
2017-11-11 14:24:49 -0500

    updates console comment

--- src/components/ul.js
+++ src/components/ul.js
@@ -3,7 +3,7 @@ import './ul.css';
 import Li from './li';
 
 export default function Ul(props) {
-	console.log(props.list);
+	console.log('UL props =>',props.list);
 
 	const listOWords = props.list.map((list, ind) =>
 		<Li key={ind} word={list.title} boldWord={list.Orator}/>
