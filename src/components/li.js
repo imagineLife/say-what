0d143e934f802d4863d5978076986c2c3bcf6eee
Eric Laursen <ericjlaursen@gmail.com>
2017-11-11 20:24:07 -0500

    updates conditional rendering

--- src/components/li.js
+++ src/components/li.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './li.css';
 
 export default function Li(props) {
-	let objSize = Object.keys(props).length;
-
+	let objSize = Object.keys(props.boldWord).length;
+	console.log('li props =>',props,'objSize =>',objSize);
 	switch(objSize) {
-		case 2 :
+		case 1 :
 		    return (
 				<li className="li"><span className="boldWord">{props.boldWord}-Letter Words : </span> {props.word}</li>
 			);
 
 		default :
 		    return (
-				<li>{props.word}</li>
+				<li className="li"><span className="boldWord">{props.boldWord} : </span> {props.word}</li>
 		    );
 	}
 }
