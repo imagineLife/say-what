278c1cf3bf96bd47c2e082b7f01bc2986db04645
Eric Laursen <ericjlaursen@gmail.com>
2017-11-11 20:22:56 -0500

    makes var names more user-friendly

--- src/components/d3/gauge.js
+++ src/components/d3/gauge.js
@@ -11,11 +11,11 @@ export default class GaugeChart extends React.Component {
 	//Spread props makes extensibility!!
 		this.state = {...props,
 			powerGauge : this.gauge(`#gcSVG${props.sectionKey}`, {
-				size: 300,
-				clipWidth: 300,
-				clipHeight: 300,
-				ringWidth: 60,
-				maxValue: props.dataKey.wordCount,
+				totalRadius: 300,
+				arcTotalWidth: 300,
+				arcTotalHeight: 300,
+				ringThickness: 60,
+				maxRingValue: props.dataKey.wordCount,
 				transitionMs: 4000,
 			})
 		}
@@ -38,36 +38,43 @@ export default class GaugeChart extends React.Component {
 		console.log('CompDidCatch err...',err,info);
 	}
 
+/*
+	set var that is a fn on a container & configuration
+		- in this case, container is 
+			<svg id={ `gcSVG${this.props.sectionKey}` }></svg>
+		- configuration  is the setup of the chart
 
+	HERE is a default configuration, above is more-custom configuration
+*/
 	gauge = (container, configuration) =>  {
 		let that = {};
 		let config = {
-			size						: 710,
-			clipWidth					: 200,
-			clipHeight					: 110,
-			ringInset					: 20,
-			ringWidth					: 20,
+			totalRadius						: 710,
+			arcTotalWidth					: 200,
+			arcTotalHeight					: 110,
+			outerRad					: 20,
+			ringThickness				: 20,
 			
 			pointerWidth				: 10,
 			pointerTailLength			: 5,
 			pointerHeadLengthPercent	: 0.9,
 			
-			minValue					: 0,
-			maxValue					: 10,
+			minRingVal					: 0,
+			maxRingValue				: 10,
 			
 			minAngle					: -90,
 			maxAngle					: 90,
 			
 			transitionMs				: 750,
 			
-			majorTicks					: 5,
+			majorTicks					: 8,
 			labelFormat					: d3.format('d'),
 			labelInset					: 10,
 			
 			arcColorFn					: d3.interpolateHsl(d3.rgb('#e8e2ca'), d3.rgb('#3e6c0a'))
 		};
 		let range = undefined;
-		let r = undefined;
+		let radius = undefined;
 		let pointerHeadLength = undefined;
 		
 		let svg = undefined;
@@ -89,20 +96,22 @@ export default class GaugeChart extends React.Component {
 			}
 			
 			range = config.maxAngle - config.minAngle;
-			r = config.size / 2;
-			pointerHeadLength = Math.round(r * config.pointerHeadLengthPercent);
+			radius = config.totalRadius / 2;
+			pointerHeadLength = Math.round(radius * config.pointerHeadLengthPercent);
 
-			// a linear scale that maps domain values to a percent from 0..1
+// a linear scale that maps domain values to a percent from 0..1
 			scale = d3.scaleLinear()
 				.range([0,1])
-				.domain([config.minValue, config.maxValue]);
-				
+				.domain([config.minRingVal, config.maxRingValue]);
+	
+//ticks = arc colored-sections 
 			ticks = scale.ticks(config.majorTicks);
 			tickData = d3.range(config.majorTicks).map(function() {return 1/config.majorTicks;});
 			
+//SHAPE & Stats of the arc		
 			arc = d3.arc()
-				.innerRadius(r - config.ringWidth - config.ringInset)
-				.outerRadius(r - config.ringInset)
+				.innerRadius(radius - config.ringThickness - config.outerRad)
+				.outerRadius(radius - config.outerRad)
 				.startAngle(function(d, i) {
 					let ratio = d * i;
 					return deg2rad(config.minAngle + (ratio * range));
@@ -112,63 +121,62 @@ export default class GaugeChart extends React.Component {
 					return deg2rad(config.minAngle + (ratio * range));
 				});
 		}
+
 		that.configure = configure;
-		
+
 		function centerTranslation() {
-			return 'translate('+r +','+ r +')';
+			return 'translate('+ radius +','+ radius +')';
 		}
 		
 		function isRendered() {
 			return (svg !== undefined);
 		}
+
 		that.isRendered = isRendered;
 		
 		function render(newValue) {
+//declare svg 			
 			svg = d3.select(container)
 				.append('svg:svg')
 					.attrs({
 						'class' : 'gauge',
-						'width' : config.clipWidth,
-						'height' : config.clipHeight,
-						'border' : '1px solid black'
+						'width' : config.arcTotalWidth,
+						'height' : config.arcTotalHeight
+
 					});
 			
-			let centerTx = centerTranslation();
+			let centerTransTxt = centerTranslation();
 			
 			let arcs = svg.append('g')
 					.attrs({
 						'class': 'arc',
-						'transform': centerTx,
-						'border' : '1px solid black'
-						});
+						'transform': centerTransTxt
+					});
 			
 			arcs.selectAll('path')
 					.data(tickData)
 				.enter().append('path')
 					.attrs({
-						'fill': function(d, i) {
-							return config.arcColorFn(d * i);
-						},
-						'd' : arc,
-						'border':'1px solid green'
+						'fill': (d, i) => { return config.arcColorFn(d * i); },
+						'd' : arc
 					});
 			
-			let lg = svg.append('g')
-					.attrs({
-						'class': 'label',
-						'transform': centerTx,
-						'border': '1px solid green'
-					});
+			// let lg = svg.append('g')
+			// 		.attrs({
+			// 			'class': 'label',
+			// 			'transform': centerTx,
+			// 			'border': '1px solid green'
+			// 		});
 
-			lg.selectAll('text')
-					.data(ticks)
-				.enter().append('text')
-					.attr('transform', function(d) {
-						let ratio = scale(d);
-						let newAngle = config.minAngle + (ratio * range);
-						return 'rotate(' +newAngle +') translate(0,' +(config.labelInset - r) +')';
-					})
-					.text(config.labelFormat);
+			// lg.selectAll('text')
+			// 		.data(ticks)
+			// 	.enter().append('text')
+			// 		.attr('transform', function(d) {
+			// 			let ratio = scale(d);
+			// 			let newAngle = config.minAngle + (ratio * range);
+			// 			return 'rotate(' +newAngle +') translate(0,' +(config.labelInset - r) +')';
+			// 		})
+			// 		.text(config.labelFormat);
 
 			let lineData = [ [config.pointerWidth / 2, 0], 
 							[0, -pointerHeadLength],
@@ -176,44 +184,47 @@ export default class GaugeChart extends React.Component {
 							[0, config.pointerTailLength],
 							[config.pointerWidth / 2, 0] ];
 
-			console.log('lineData =>',lineData);
+			// console.log('lineData =>',lineData);
 
 			let pointerLine = d3.line().curve(d3.curveLinear);
 
-			let pg = svg.append('g').data([lineData])
+			let pointerG = svg.append('g').data([lineData])
 					.attrs({
 						'class': 'pointer',
-						'transform' : centerTx
+						'transform' : centerTransTxt
 					});
 					
-			pointer = pg.append('path')
-				.attrs({
-					'd': pointerLine,
-					'transform': 'rotate(' +config.minAngle +')'
-				});
+			// pointer = pointerG.append('path')
+			// 	.attrs({
+			// 		'd': pointerLine,
+			// 		'transform': 'rotate(' +config.minAngle +')'
+			// 	});
 				
-			setNeedleVal(newValue === undefined ? 0 : newValue);
+			// setNeedleVal(newValue === undefined ? 0 : newValue);
 		}
 		
 		that.render = render;
 		
 		function setNeedleVal(newValue, newConfiguration) {
-			console.log('setting NeedleVal, newVal =>',newValue,'newConfig =>',newConfiguration);
+			// console.log('setting NeedleVal, newVal =>',newValue,'newConfig =>',newConfiguration);
 			if ( newConfiguration  !== undefined) {
 				configure(newConfiguration);
 			}
 			let ratio = scale(newValue);
 			let newAngle = config.minAngle + (ratio * range);
-			pointer.transition()
-				.duration(config.transitionMs)
-				.ease(d3.easeElastic)
-				.attr('transform', 'rotate(' +newAngle +')');
+			// pointer.transition()
+			// 	.duration(config.transitionMs)
+			// 	.ease(d3.easeElastic)
+			// 	.attr('transform', 'rotate(' +newAngle +')');
 		}
 		
 		that.setNeedleVal = setNeedleVal;
 
 		configure(configuration);
-		
+
+		console.log('state is',this.state);
+
+
 		return that;
 	};
 	
@@ -221,7 +232,9 @@ export default class GaugeChart extends React.Component {
 
 	shouldComponentUpdate() { return false }
 	
-	render(){    
+	render(){
+			console.log('STATE =>',this.state);
+
     	return (
     			<svg id={ `gcSVG${this.props.sectionKey}` }></svg>
     	);
