367cb2221b020912b91820019ca04fe3eb8c717c
Eric Laursen <ericjlaursen@gmail.com>
2017-11-16 08:05:38 -0500

    includes renamed file(s)

---
+++ src/components/d3/bubbleChart/Bubbles/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Bubbles.css';
+
+export default ({margins, svgDimensions }) => {
+  const { height, width } = svgDimensions
+
+  return (
+    <g className='bubbleWrapper'>
+    </g>
+  )
+}
