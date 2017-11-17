2cd65fd52013c5f0121f6d324acb9c494d226daf
Eric Laursen <ericjlaursen@gmail.com>
2017-11-16 19:06:49 -0500

    removes dev code

--- src/components/d3/bubbleChart/Bubbles/index.js
+++ src/components/d3/bubbleChart/Bubbles/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import './Bubbles.css';
+import React, { Component } from 'react'
+import './Bubbles.css'
 
-export default ({margins, svgDimensions }) => {
-  // const { height, width } = svgDimensions
+export default class Bubbles extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-  return (
-    <g className='bubbleWrapper'>
-    </g>
-  )
+
+  render() {
+	console.log('Bubbles props',this.props);
+    const { scales, margins, data, svgDimensions } = this.props
+    const { height, width } = svgDimensions
+
+    const bubbles = (
+      data.map(data =>
+        <g key={Object.keys(data)} className="bubbleWrapper">
+         <p>Bubbles go here</p>
+        </g>,
+      )
+    )
+
+    return (
+      <g>{bubbles}</g>
+    )
+  }
 }
