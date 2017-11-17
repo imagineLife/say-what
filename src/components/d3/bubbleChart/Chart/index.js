e6ab600be9dd36ff9ebcc5370429a42ea5846a69
Eric Laursen <ericjlaursen@gmail.com>
2017-11-17 06:08:42 -0500

    reconfigs chat output to majorityD3

--- src/components/d3/bubbleChart/Chart/index.js
+++ src/components/d3/bubbleChart/Chart/index.js
@@ -1,36 +1,107 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3';
-import 'd3-selection-multi';
 import Bubbles from '../Bubbles'
 import ResponsiveWrapper from '../../../ResponsiveWrapper'
 import './Chart.css'
 
 
 class Chart extends Component {
-  constructor(props) {
-    super(props)
-    this.xScale = d3.scaleBand()
-    this.yScale = d3.scaleLinear()
-  }
+  // constructor(props) {
+  //   super(props)
+  // }
 
   render() {
-//chart margins / offset
-    const margins = { top: 5, right: 20, bottom: 60, left: 30 }
 
-//svgDimensions gets its withd from this.props.parentWidth
-  // which SEEMS to come from the responsiveWrapper fn
-  //  which wraps around the <Chart /> 
-    const svgDimensions = {
-      width: Math.max(this.props.parentWidth, 300),
-      height: 450
+    let myData = [
+      {'pickles':25},
+      {"cucumbers": 14},
+      {"iceCubes": 7}
+    ];
+
+    let d = {};
+    let classes;
+    classes = myData;
+
+    var container = d3.select(".Responsive-wrapper");
+
+    //Declare SVG
+    var svgElm = d3.select(".bubbleSVG"),
+        svgWidth = +container.style("width").replace('px',''),
+        svgHeight = +container.style("height").replace('px','');
+
+        svgElm.attr('viewBox', "0, 0, " + svgWidth + ", " + svgHeight)  // min-x, min-y, width, height
+        .attr('preserveAspectRatio', "xMinYMid");
+
+    var format = d3.format(",d");
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+    var pack = d3.pack()
+        .size([svgWidth, svgHeight])
+        .padding(1.5);
+
+    myData.forEach((obj) =>{
+      d.value = Object.values(obj);
+      let objKey = Object.keys(obj);
+      let objVal = Object.values(obj);
+
+    //declare ROOT?!
+      var root = d3.hierarchy({children: classes})
+          .sum(function(d) {
+            return Object.values(d); 
+          });
+
+    //declare the bubble element
+    //give it a class
+    //make it transform
+      var bubble = svgElm.selectAll(".bubble")
+        .data(pack(root).leaves())
+        .enter().append("g")
+          .attr("class", "bubble")
+          .attr("transform", function(d) { 
+            return "translate(" + d.x + "," + d.y + ")"; 
+          });
+
+      bubble.append("circle")
+          .attr("id", function(d) { return d.id; })
+          .attr("r", function(d) { return d.r; })
+          .style("fill", function(d) { return color(d.package); });
+
+
+      bubble.append("clipPath")
+          .attr("id", function(d) { return "clip-",Object.keys(d.data)})
+        .append("use")
+          .attr("xlink:href", function(d) { return "#" + Object.keys(d.data) });
+
+
+      bubble.append("text")
+          .attr("clip-path", function(d) { return "url(#clip-"+Object.keys(d.data)+")"; })
+        .selectAll("tspan")
+        .data(function(d) { return Object.keys(d.data); })
+        .enter().append("tspan")
+          .attr("x", 0)
+          .attr("y", function(d,i,letters) {
+            // console.log(d, i, letters);
+           return 13 + (i - letters.length / 2 - 0.5) * 10; })
+          .text(function(d) { return d+'\n'+objVal; });
+
+      bubble.append("title")
+          .text(function(d) { return Object.keys(d.data)[0] + "\n" + format(d.value); });
+    });
+
+    setContainerHeight();
+
+    d3.select(window).on('resize', setContainerHeight);
+
+    function setContainerHeight() {
+      var w = parseInt( container.style("width"), 10); // computed width
+      var a = svgWidth / svgHeight; // = aspect ratio to be applied to the container
+      console.log(a, w);
+      svgElm.attr('height', w / a  + 'px');
     }
 
     return (
-      <svg className='bubbleSVG' width={svgDimensions.width} height={svgDimensions.height}>
-        <Bubbles
-          margins={margins}
-          svgDimensions={svgDimensions}
-        />
+      <svg className='bubbleSVG'>
       </svg>
     )
   }
