702cb7e0a92d2a41183b5eb229ba06174ad677c3
Eric Laursen <ericjlaursen@gmail.com>
2017-11-21 17:13:10 -0500

    add ul class and reconig capitalize words

--- src/components/ul.js
+++ src/components/ul.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './ul.css';
 import Li from './li';
+import LiNoBold from './liNoBold';
 
 export default function Ul(props) {
 	console.log('UL props =>',props.list);
+	let listOWords = [];
+	let capd = '';
+	let capdWords = [];
+	let singleWord = props.list;
 
-	const listOWords = props.list.map((list, ind) =>
-		<Li key={ind} word={list.title} boldWord={list.Orator}/>
-	)
+//capitalize words fn
+	let capitalize = (word) => {
+		capd = word.charAt(0).toUpperCase() + word.slice(1);
+		capdWords.push(capd);
+		return capd;
+
+	}
+
+//capitalize each word
+	for(let i=0; i < singleWord.length; i++){
+		capitalize(singleWord[i]);
+	}
+
+	if(props.list.title){
+		listOWords = props.list.map((list, ind) =>
+			<Li key={ind} word={list.title} boldWord={list.Orator}/>
+		)
+	}else{
+		listOWords = capdWords.map((list, ind) =>
+			<LiNoBold key={ind} word={list}/>
+		)
+	}
 
     return (
-		<ul>
+		<ul className="centeredList">
 			{listOWords}
 		</ul>
     );
