56b9ce52d21f1bdc271c66e7ab812e0f16ac3d71
Eric Laursen <ericjlaursen@gmail.com>
2017-12-10 12:28:32 -0500

    reconfigs fetch url to default rather than speechID.

--- src/containers/SpeechText/state/actions.js
+++ src/containers/SpeechText/state/actions.js
@@ -1,6 +1,7 @@
 export const GET_SPEECH_TEXT = 'GET_SPEECH_TEXT';
 export const xNgetSpeechText = (speechID, dispatch) => {
 	//This action pulls the URL string of the speech rom the DB
-	fetch(`http://localhost:8080/api/speeches/text/${speechID}`)
+//	fetch(`http://localhost:8080/api/speeches/text/${speechID}`)
+	fetch(`http://localhost:8080/api/speeches/default`)
 	.then((obj) => console.log('action object->',obj.json()));
 };
