138adc2d502d663fa4e597543d2cc335415dc770
Eric Laursen <ericjlaursen@gmail.com>
2017-12-10 12:18:24 -0500

    Renames action variable.

--- src/containers/SpeechText/state/actions.js
+++ src/containers/SpeechText/state/actions.js
@@ -1,5 +1,6 @@
 export const GET_SPEECH_TEXT = 'GET_SPEECH_TEXT';
-export const getSpeechText = (speechID, dispatch) => {
+export const xNgetSpeechText = (speechID, dispatch) => {
+	//This action pulls the URL string of the speech rom the DB
 	fetch(`http://localhost:8080/api/speeches/text/${speechID}`)
-	.then((obj) => console.log(obj.json()));
+	.then((obj) => console.log('action object->',obj.json()));
 };
