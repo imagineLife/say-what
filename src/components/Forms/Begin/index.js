c30a42b464bbd0435f7db24e39102052e343394a
Eric Laursen <ericjlaursen@gmail.com>
2017-12-15 06:12:46 -0500

    Makes Trump Speech text the default instead of random speech.

--- src/components/Forms/Begin/index.js
+++ src/components/Forms/Begin/index.js
@@ -3,11 +3,12 @@ import './BeginForm.css';
 import {Link} from 'react-router-dom';
 
 export default function BeginForm(props) {
+  console.log('BeginForm props->',props);
 
   return (
 		<form className='begin-form'>
       <div>
-        <label htmlFor="getStarted">See <em>a random speech </em></label>
+        <label htmlFor="getStarted">See Trumps Inauguration Speech</label>
         <button type='button' id="getStarted">
           <Link to={`/speechData/5a1ad99f978ca2681f42df12`}>
               Here!
