91a1906a4422c505221deb51bcc04b9b16fe6944
Eric Laursen <ericjlaursen@gmail.com>
2017-12-15 06:09:21 -0500

    Includes store configuration function and initial state.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import store from './store';
+import myConfigureStore from './store';
 import './index.css';
 
-// const initialState = {};
+const initialState = {
+	speechID : '5a1ad99f978ca2681f42df12'
+};
 
-// const store = createStore(state => state, initialState);
+const store = myConfigureStore(initialState);
 
 ReactDOM.render(
 	<Provider store={store}>
