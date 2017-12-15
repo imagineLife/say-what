e1d1b7f8e57f830af8a2441a85a20361aa821299
Eric Laursen <ericjlaursen@gmail.com>
2017-12-15 06:06:19 -0500

    Converts Splash from function to class.

--- src/containers/Splash/index.js
+++ src/containers/Splash/index.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import './Splash.css';
 import HeaderSplash from '../../components/HeaderSplash';
 import Section from '../../components/Section';
 
-export default function App(props) {
-
-		const pageHeader = {
-			Title: `Say What?!`,
-			subTitle: `See data within speeches`
+class Splash extends Component {
+		constructor(props){
+			super(props);
 		}
 
-		const sectionsArray =[
-			{
-				title: `Notice what they're saying`,
-				text: `SayWhay looks at a speech and its text content as a dataset, illuminating the largest words spoken, the most common words of the speech, and more!`
-			},
-			{
-				title: `Pick from a variety of Speeches`,
-				text: `Interested in the details of what Trump said? Maybe Hillary? MLK Jr? Perhaps JFK or Abraham Lincoln?
-				Get insight into the words of a variety of speeches.`,
-				img: 'IMAGE OF SPEAKER'
-			},
-			{
-				title: 'Check it out!',
-				includeBeginForm: true,
-				includeBottomSpace:true
+		render(){
+			console.log('Splash props ->',this.props);
+
+			const pageHeader = {
+				Title: `Say What?!`,
+				subTitle: `See data within speeches`
 			}
-		];
-		
-		const sections = sectionsArray.map((sec,ind) => {
-	      	return <Section key={ind} {...sec}/>;
-		})
+
+			const sectionsArray =[
+				{
+					title: `Notice what they're saying`,
+					text: `SayWhay looks at a speech and its text content as a dataset, illuminating the largest words spoken, the most common words of the speech, and more!`
+				},
+				{
+					title: `Pick from a variety of Speeches`,
+					text: `Interested in the details of what Trump said? Maybe Hillary? MLK Jr? Perhaps JFK or Abraham Lincoln?
+					Get insight into the words of a variety of speeches.`,
+					img: 'IMAGE OF SPEAKER'
+				},
+				{
+					title: 'Check it out!',
+					includeBeginForm: true,
+					includeBottomSpace:true,
+					speechID: this.props.speechID
+				}
+			];
+			
+			const sections = sectionsArray.map((sec,ind) => {
+		      	return <Section key={ind} {...sec}/>;
+			})
 
 
-    return (
-		<main role="main">
-		  <HeaderSplash title={pageHeader.Title} subTitle={pageHeader.subTitle} />
-	      
-	      {sections} 
+		    return (
+				<main role="main">
+				  <HeaderSplash title={pageHeader.Title} subTitle={pageHeader.subTitle} />
+			      
+			      {sections} 
 
-	    </main>
-    );
+			    </main>
+		    );
+		}
 }
+
+const mapStateToProps = (state) => ({ 
+	speechID: state._root.entries["0"][1]
+})
+
+export default connect(mapStateToProps)(Splash);
