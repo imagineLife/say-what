a9eae1a02ac4c6a61f063c5dd7ff230547fdfcfc
Eric Laursen <ericjlaursen@gmail.com>
2017-12-16 14:36:11 -0500

    Comments "Useless Constructor".

--- src/containers/Splash/index.js
+++ src/containers/Splash/index.js
@@ -5,9 +5,9 @@ import HeaderSplash from '../../components/HeaderSplash';
 import Section from '../../components/Section';
 
 class Splash extends Component {
-		constructor(props){
-			super(props);
-		}
+		// constructor(props){
+		// 	super(props);
+		// }
 
 		render(){
 			console.log('Splash props ->',this.props);
