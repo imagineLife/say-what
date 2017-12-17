f6a3b9a8d69c1127bb99ca74bd9300a96aa1646c
Eric Laursen <ericjlaursen@gmail.com>
2017-12-17 18:30:35 -0500

    Converted to Class and introduces dev Submite & console.log.

--- src/components/Forms/Register/index.js
+++ src/components/Forms/Register/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './RegisterForm.css';
 
-export default function Register(props) {
+export default class Register extends React.Component {
+  submitFn(ev){
+    ev.preventDefault();
+    console.log('submitted');
+  }
 
-  return (
-    <form className='register-form'>
-      <fieldset>
-        <legend>Register</legend>
-          <input id="firstName" type="text" name="firstName" placeholder="firstName" required/>
-          <input id="lastName" type="text" name="lastName" placeholder="lastName" required/>
-          <input id="userName" type="text" name="userName" placeholder="userName" required/>
-          <input id="passWord" type="text" name="passWord"  placeholder="PassWord" required/>
-      </fieldset>
-          <input type="submit" name="submit" value="Register" />
-    </form>
-  );
+  render(){
+    return (
+      <form className='register-form' onSubmit={e => this.submitFn(e)}>
+        <fieldset>
+          <legend>Register</legend>
+            <input id="firstName" type="text" name="firstName" placeholder="firstName" required/>
+            <input id="lastName" type="text" name="lastName" placeholder="lastName" required/>
+            <input id="userName" type="text" name="userName" placeholder="userName" required/>
+            <input id="passWord" type="text" name="passWord"  placeholder="PassWord" required/>
+        </fieldset>
+            <input type="submit" name="submit" value="Register" />
+      </form>
+    );
+  }
 }
