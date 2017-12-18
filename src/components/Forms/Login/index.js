14fba19c7a5c6ca663f887a0c1a40a69c7df001c
Eric Laursen <ericjlaursen@gmail.com>
2017-12-18 08:10:55 -0500

    Successfully runs login function.

--- src/components/Forms/Login/index.js
+++ src/components/Forms/Login/index.js
@@ -12,9 +12,40 @@ export default class LoginForm extends React.Component {
     }
   }
 
+  getResFromAPI(ev){
+    ev.preventDefault();
+    let tempState = this.state;
+    // console.log(tempState);
+    return (
+        fetch(`http://localhost:8080/api/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tempState)
+        })
+            // Reject any requests which don't return a 200 status, creating
+            // errors which follow a consistent format
+            .then(res => res.json())
+            // .then(({authToken}) => storeAuthInfo(authToken, dispatch))
+            .catch(err => {
+                const {code} = err;
+                if (code === 401) {
+                  console.log(code);
+                    // Could not authenticate, so return a SubmissionError for Redux Form
+                    // return Promise.reject(
+                    //     new SubmissionError({
+                    //         _error: 'Incorrect username or password'
+                    //     })
+                    // );
+                }
+            })
+    );    
+  }
+
   render(){
     return (
-    	<form className='login-form'>
+    	<form className='login-form' onSubmit={e => this.getResFromAPI(e)}>
         <fieldset>
           <legend>Log in</legend>
             <input id="userame" type="text" name="username" placeholder="Username" required/>
