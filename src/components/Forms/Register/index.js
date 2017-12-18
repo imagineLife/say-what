84d7d7d950c61cccec00e26d88de95a702b051c8
Eric Laursen <ericjlaursen@gmail.com>
2017-12-17 22:00:54 -0500

    Converts inputs to controlled, and sets vals to state.

--- src/components/Forms/Register/index.js
+++ src/components/Forms/Register/index.js
@@ -2,9 +2,45 @@ import React from 'react';
 import './RegisterForm.css';
 
 export default class Register extends React.Component {
-  submitFn(ev){
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      passWord: ''
+    }
+  }
+
+  submitFn(ev, values){
     ev.preventDefault();
-    console.log('submitted');
+    console.log('state->',this.state);
+  }
+
+  setText(text, id) {
+    switch(id) {
+      case 'firstName' :
+        this.setState({
+          firstName: text
+        })
+        break;
+      case 'lastName' :
+        this.setState({
+          lastName: text
+        })
+        break;
+      case 'userName' :
+        this.setState({
+          userName: text
+        })
+        break; 
+      default:
+        this.setState({
+          passWord: text
+        })
+        break;
+    }
+
   }
 
   render(){
@@ -12,11 +48,39 @@ export default class Register extends React.Component {
       <form className='register-form' onSubmit={e => this.submitFn(e)}>
         <fieldset>
           <legend>Register</legend>
-            <input id="firstName" type="text" name="firstName" placeholder="firstName" required/>
-            <input id="lastName" type="text" name="lastName" placeholder="lastName" required/>
-            <input id="userName" type="text" name="userName" placeholder="userName" required/>
-            <input id="passWord" type="text" name="passWord"  placeholder="PassWord" required/>
-        </fieldset>
+            <input 
+              id="firstName" 
+              type="text" 
+              name="firstName" 
+              placeholder="firstName"
+              value={this.state.firstName}
+              onChange={e => this.setText(e.target.value, e.target.id)}
+            required/>
+            <input 
+              id="lastName" 
+              type="text" 
+              name="lastName" 
+              placeholder="lastName"
+              value={this.state.lastName}
+              onChange={e => this.setText(e.target.value, e.target.id)}
+            required/>
+            <input 
+              id="userName" 
+              type="text" 
+              name="userName" 
+              placeholder="userName"
+              value={this.state.userName}
+              onChange={e => this.setText(e.target.value, e.target.id)}
+            required/>
+            <input 
+              id="passWord" 
+              type="text" 
+              name="passWord" 
+              placeholder="passWord"
+              value={this.state.passWord}
+              onChange={e => this.setText(e.target.value, e.target.id)}
+            required/>
+            </fieldset>
             <input type="submit" name="submit" value="Register" />
       </form>
     );
