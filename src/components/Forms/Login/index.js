45d59ccf9fca8e9d27c22283d3796d49b2cdc892
Eric Laursen <ericjlaursen@gmail.com>
2017-12-18 07:56:26 -0500

    Converts Form from function to class.

--- src/components/Forms/Login/index.js
+++ src/components/Forms/Login/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import './LoginForm.css';
 
-export default function LoginForm(props) {
-  return (
-  	<form className='login-form'>
-      <fieldset>
-        <legend>Log in</legend>
-          <input id="userame" type="text" name="username" placeholder="Username" required/>
-          <input id="passeord" type="text" name="password"  placeholder="Password" required/>
-      </fieldset>
-          <input type="submit" name="submit" value="Log in" />
-          <label htmlFor="register">Don't have an account?</label>
-          <input type="button" name="register" id="register" value="Sign up" onClick={props.toggleForm}/>
-    </form>
-  );
+export default class LoginForm extends React.Component {
+  constructor(props){
+    console.log(props);
+    super(props);
+    
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  render(){
+    return (
+    	<form className='login-form'>
+        <fieldset>
+          <legend>Log in</legend>
+            <input id="userame" type="text" name="username" placeholder="Username" required/>
+            <input id="passeord" type="text" name="password"  placeholder="Password" required/>
+        </fieldset>
+            <input type="submit" name="submit" value="Log in" />
+            <label htmlFor="register">Don't have an account?</label>
+            <input type="button" name="register" id="register" value="Sign up" onClick={this.props.toggleForm}/>
+      </form>
+    );
+  }
 }
