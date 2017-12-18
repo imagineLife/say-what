f326d8500efcd35f40ac9f7106c96823ea5071a9
Eric Laursen <ericjlaursen@gmail.com>
2017-12-18 07:50:06 -0500

    Updates field names, no camelCase.

--- src/components/Forms/Login/index.js
+++ src/components/Forms/Login/index.js
@@ -6,8 +6,8 @@ export default function BeginForm(props) {
   	<form className='login-form'>
       <fieldset>
         <legend>Log in</legend>
-          <input id="userName" type="text" name="userName" placeholder="UserName" required/>
-          <input id="passWord" type="text" name="passWord"  placeholder="PassWord" required/>
+          <input id="userame" type="text" name="username" placeholder="Username" required/>
+          <input id="passeord" type="text" name="password"  placeholder="Password" required/>
       </fieldset>
           <input type="submit" name="submit" value="Log in" />
           <label htmlFor="register">Don't have an account?</label>
