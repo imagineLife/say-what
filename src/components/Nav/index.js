641873eafa39e1773da8ee69499c0f49d44110b2
Eric Laursen <ericjlaursen@gmail.com>
2017-12-28 16:03:28 -0500

    Converts <a>s to <Link>s.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Nav.css';
+import {Link} from 'react-router-dom';
 
 export default function Nav() {
 	//Help update this specific navLink text to change when signed-in-or-not :) 
@@ -7,22 +8,22 @@ export default function Nav() {
 	// const navLink = (true === true) 
 	const navLink = 
 						// ? (<a href="/login"><img src="/imgs/user.ico" className="icon" alt="Sign Up" /><span className="menuLabel">Sign Up</span></a>)
-						(<a href="/login"><img src="/imgs/user.ico" className="icon" alt="Sign Up" /><span className="menuLabel">Sign Up</span></a>)
+						(<Link to="/login"><img src="/imgs/user.ico" className="icon" alt="Sign Up" /><span className="menuLabel">Sign Up</span></Link>)
 						// : (<a href="/speechPicker"><img src="/imgs/user.ico" className="icon" alt="SomethingElse" /><span className="menuLabel">SomethingElse</span></a>)
     return (
 		<nav>
 			<ul>
 				<li className='liWithIcon'>
-					<a href='/'>
+					<Link to="/">
 						<img src={process.env.PUBLIC_URL+'/imgs/home.ico'} className="icon" alt='home' width="50" height="auto"/>
 						<span className="menuLabel">Home</span>
-					</a>
+					</Link>
 				</li>
 				<li className='liWithIcon'>
-					<a href='/speechData/5a1ad99f978ca2681f42df12'>
+					<Link to='/speechData/5a1ad99f978ca2681f42df12'>
 						<img src={process.env.PUBLIC_URL+'/imgs/search.ico'} className="icon" alt='View A Speech' />
 						<span className="menuLabel">View A Speech</span>
-					</a>
+					</Link>
 				</li>
 				<li className='liWithIcon'>
 					{navLink}
