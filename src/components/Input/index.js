7676d9a24d4d3eee1fc338bead9a50aad3f6fb98
Eric Laursen <ericjlaursen@gmail.com>
2018-01-03 07:32:58 -0500

    First commit of Input component file.

---
+++ src/components/Input/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Input.css';
+
+export default function Input(props) {
+	console.log(props);
+
+    return (
+	    <input 
+	      id = {props.source} 
+	      type = "text" 
+	      name = {props.source} 
+	      placeholder = {props.source.charAt(0).toUpperCase() + props.source.slice(1)} 
+	      onChange={e => props.onChangeProp(e)}
+	    required/>
+	);
+
+}
