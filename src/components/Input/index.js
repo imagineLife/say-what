04a68c67f355e72e5ee9699b63168ea14557f388
Eric Laursen <ericjlaursen@gmail.com>
2018-01-07 14:17:01 -0500

    Sets type from props.type.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,7 +6,7 @@ export default function Input(props) {
     return (
 	    <input 
 	      id = {props.source} 
-	      type = {props.source} 
+	      type = {props.type} 
 	      name = {props.source} 
 	      placeholder = {props.placeholder ? props.placeholder : props.source.charAt(0).toUpperCase() + props.source.slice(1)} 
 	      onChange={e => props.onChangeProp(e)}
