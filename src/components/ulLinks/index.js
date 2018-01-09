60bc5b09b02f3d6525dd2c5ce0b49f8fc5f8becf
Eric Laursen <ericjlaursen@gmail.com>
2018-01-08 20:54:45 -0500

    Includes speech Date in link text.

--- src/components/ulLinks/index.js
+++ src/components/ulLinks/index.js
@@ -4,14 +4,21 @@ import Li from '../li';
 import {Link} from 'react-router-dom'
 
 export default function Ul(props) {
+     let parseDateToYear = (dateToParse) => {
+	    let usableDate = new Date(dateToParse);
+		let year = (usableDate.getFullYear() < 10 ? ('0'+usableDate.getFullYear()) : usableDate.getFullYear());
+		return year;	
+     }
 
-	const speechLinks = props.speechParts.map((speech, ind) => 
+	const speechLinks = props.speechParts.map((speech, ind) =>
+
 		<Link className="ulLink" key={ind} to={`speechData/${speech._id}`} >
-			<Li word={speech.title} boldWord={speech.Orator} />
+			<Li word={`${parseDateToYear(speech.Date)} ${speech.title}`} boldWord={speech.Orator} />
 		</Link>
 	)
 
     return (
+
 		<ul>
 			{speechLinks}
 		</ul>
