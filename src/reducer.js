52f77e6066301a6421f871aa375fd9c41af25024
Eric Laursen <ericjlaursen@gmail.com>
2018-01-10 06:41:58 -0500

    Includes requestReducer.

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,12 @@
 // import { fromJS } from 'immutable'; 
 import { combineReducers } from 'redux-immutable';
 import loginReducer from './components/Forms/Login/state/reducer';
+import requestReducer from './components/Forms/Request/state/reducer';
 
 export default function createReducer(asyncReducers) { 
 	return combineReducers({
 		user: loginReducer,
+		request: requestReducer,
 		...asyncReducers,
 	});
 }
