24f86aff124c910562e861c8f81456367c965f9f
Eric Laursen <ericjlaursen@gmail.com>
2018-01-13 11:17:12 -0500

    Includes maxWidthWrapper div, for big-screen usage.

--- src/components/Forms/Begin/index.js
+++ src/components/Forms/Begin/index.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import '../Forms.css';
 import './BeginForm.css';
 import {Link} from 'react-router-dom';
 
 export default function BeginForm(props) {
   return (
-		<form className='begin-form'>
-      <div>
-        <label htmlFor="getStarted">See Trumps Inauguration Speech</label>
-          <Link to={`/speechData/default`}>
-            <button type='button' id="getStarted">
-              Here!
-            </button>
-          </Link>
-      </div>
-      <div>
-        <label htmlFor="pickFromList">Choose a speech </label>
-          <Link to={`/speechPicker`}>
-            <button type='button' id ="pickFromList">
-              Here!
-            </button>
-          </Link>
-      </div>
-    </form>
+    <div className="maxWidthWrapper">
+  		<form className='begin-form'>
+        <div>
+          <label htmlFor="getStarted">See Trumps Inauguration Speech</label>
+            <Link to={`/speechData/default`}>
+              <button type='button' id="getStarted">
+                Here!
+              </button>
+            </Link>
+        </div>
+        <div>
+          <label htmlFor="pickFromList">Choose a speech </label>
+            <Link to={`/speechPicker`}>
+              <button type='button' id ="pickFromList">
+                Here!
+              </button>
+            </Link>
+        </div>
+      </form>
+    </div>
   );
 }
