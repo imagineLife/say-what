22818b0cd5e92431990d3dea0428ce41d484609a
Eric Laursen <ericjlaursen@gmail.com>
2018-01-13 12:22:09 -0500

    Simplifies li output.

--- src/components/li/index.js
+++ src/components/li/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import './li.css';
 
 export default function Li(props) {
-	let objSize = Object.keys(props.boldWord).length;
-	// console.log('li props =>',props,'objSize =>',objSize);
-	switch(objSize) {
-		case 1 :
-		    return (
-				<li className="li"><span className="boldWord">{props.boldWord}-Letter Words : </span> {props.word}</li>
-			);
-
-		default :
-		    return (
-				<li className="li"><span className="boldWord">{props.boldWord} : </span> {props.word}</li>
-		    );
-	}
+    return (
+		<li className="li">
+			<span className="boldWord">{props.boldWord}</span>
+			<br />
+			<p> {props.word} </p>
+		</li>
+	);
 }
