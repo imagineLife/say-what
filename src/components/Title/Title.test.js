e8680fdb921755a2e98063e677f47ee256698edc
Eric Laursen <ericjlaursen@gmail.com>
2018-01-14 18:54:11 -0500

    Includes smoke test.

---
+++ src/components/Title/Title.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Title from './index';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Title.js', () => {
+	
+	it('renders without crashing', () => {
+		shallow(<Title />);
+	});	
+})
