feab25295fd0ec84973fbdfb4b930b1bed64fb7d
Eric Laursen <ericjlaursen@gmail.com>
2018-01-14 10:39:00 -0500

    Remove dev lines.

--- src/App.test.js
+++ src/App.test.js
@@ -8,8 +8,6 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('App.js', () => {
 	it('renders without crashing', () => {
-	  // const div = document.createElement('div');
-	  // ReactDOM.render(<App />, div);
 	  shallow(<App />);
 	});	
 })
