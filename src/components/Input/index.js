ad874c7b86e80dd98e12a00f8856dee3c66568ce
Eric Laursen <ericjlaursen@gmail.com>
2018-01-14 21:13:54 -0500

    sets optional minimun password length to 8 characters.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,7 +7,8 @@ export default function Input(props) {
 	    <input 
 	      id = {props.source} 
 	      type = {props.type} 
-	      name = {props.source} 
+	      name = {props.source}
+	      minLength = {props.minInputLength ? props.minInputLength : null}
 	      placeholder = {props.placeholder ? props.placeholder : props.source.charAt(0).toUpperCase() + props.source.slice(1)} 
 	      onChange={e => props.onChangeProp(e)}
 	    required/>
