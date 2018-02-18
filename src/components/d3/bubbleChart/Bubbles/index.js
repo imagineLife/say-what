0a894f2a0c8494c23c694cb6c398cb50e48bb817
Eric Laursen <ericjlaursen@gmail.com>
2018-02-18 12:39:53 -0500

    Includes console.logs for development.

--- src/components/d3/bubbleChart/Bubbles/index.js
+++ src/components/d3/bubbleChart/Bubbles/index.js
@@ -8,11 +8,9 @@ export default class Bubbles extends Component {
 
 
   render() {
-	console.log('Bubbles props',this.props);
     const { scales, margins, data, svgDimensions } = this.props
     const { height, width } = svgDimensions
 
-    console.log('1st Val =>',data);
     const bubbles = (
       data.map(dataElement =>
         <circle key={Object.keys(dataElement)} className="bubble" r={Object.values(dataElement)}></circle>,
