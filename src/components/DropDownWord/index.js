247f30cddf91174ac042a4c105054340bb70b572
Eric Laursen <ericjlaursen@gmail.com>
2019-05-30 19:28:15 -0400

    Includes nested optional list

--- src/components/DropDownWord/index.js
+++ src/components/DropDownWord/index.js
@@ -4,11 +4,19 @@ import './index.css'
 export default function DropDownWord({word, wordsByOrator}){
 	let [arrowDirection, setArrowDirection] = React.useState("right")
 	return(
-		<li className="comparison-list">
-			<span className="value">{word.word}</span>
-			<span className="count">{word.occurances}<sub>x</sub></span>
-			<i 
-				className={`arrow ${arrowDirection}`} 
-				onClick={() => setArrowDirection((arrowDirection == 'right') ? 'down' : 'right')}></i>
-		</li>)
+		<React.Fragment>
+			<li className="comparison-list">
+				<span className="value">{word.word}</span>
+				<span className="count">{word.occurances}<sub>x</sub></span>
+				<i 
+					className={`arrow ${arrowDirection}`} 
+					onClick={() => setArrowDirection((arrowDirection == 'right') ? "down" : "right")}></i>
+			</li>
+			{arrowDirection == "down" && wordsByOrator.map(wbo => {
+				return (<li key={`${word.word}${wbo.orator}`} className="list-by-orator">
+					<span className="value">{wbo.orator}</span>
+					<span className="count">{wbo.occurances}<sub>x</sub></span>
+				</li>)
+			})}
+		</React.Fragment>)
 }
