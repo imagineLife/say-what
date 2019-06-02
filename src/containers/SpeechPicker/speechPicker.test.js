94d269a169fe0b19bb0e377469627cc01c8171a7
Eric Laursen <ericjlaursen@gmail.com>
2019-06-02 19:48:23 -0400

    removes local-storage mocker

--- src/containers/SpeechPicker/speechPicker.test.js
+++ src/containers/SpeechPicker/speechPicker.test.js
@@ -3,13 +3,8 @@ import ReactDOM from 'react-dom';
 import { SpeechPicker } from './index';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import localStorage from 'mock-local-storage';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-global.window = {};
-window.localStorage = global.localStorage;
-
 
 describe('SpeechPicker.js', () => {
 	window.backendPath = '//localhost:8080';
