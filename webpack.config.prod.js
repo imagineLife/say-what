5f263206ea230c6a68fca7d259e79efb405d6d57
Eric Laursen <ericjlaursen@gmail.com>
2019-06-03 10:11:40 -0400

    removes entry/output from config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,12 +3,6 @@ const MiniCss = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-  },
   module: {
     rules: [
       {
