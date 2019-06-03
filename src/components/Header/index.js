caf996df249a69549d1acd870dca3fcb5c74a3b0
Eric Laursen <ericjlaursen@gmail.com>
2019-06-03 12:50:34 -0400

    updates header syntax

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Header.css';
 
-export default function Header(props) {
-
+export default function Header({imageLinkProp, title, subTitle}) {
+  console.log('imageLinkProp')
+  console.log(imageLinkProp)
+  let myLink = 'trump.jpg'
+  
 	let sectionStyle = {
-	  background: `url(${ props.imageLinkProp }) no-repeat center center`,
+	  background: `url(/src/imgs/${myLink}) no-repeat center center`,
 	  backgroundSize: `cover`
 	};
 
     return (
       <header style={sectionStyle} role="banner" className="pageHeader">
-        <h1 className="titleH1">{props.title}</h1>
-        <h3 className="subTitle">{props.subTitle}</h3>
+        <h1 className="titleH1">{title}</h1>
+        <h3 className="subTitle">{subTitle}</h3>
       </header>
     );
 }
