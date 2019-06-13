5b997eb3af5c539fd6cee9275d0c4891c09ae2cf
Eric Laursen <ericjlaursen@gmail.com>
2019-06-13 16:59:03 -0400

    combines gradients in bars

--- src/components/d3/BarChart/Bars/index.js
+++ src/components/d3/BarChart/Bars/index.js
@@ -13,46 +13,54 @@ export default function Bars({ scales, margins, data, svgDimensions }){
     .interpolate(interpolateLab)
     .domain([0, data.length - 1]);
 
-  let ColorGradientDefs = ({color, direction}) => {
-    let directions = (direction == 'down') ? {
+  // let ColorGradientDefs = ({color, direction}) => {
+    // let directions = (direction == 'down') ? {
+      let directions = {
         x1:"0",
         x2:"0",
         y1:"1",
         y2:"0"
-      } : null
+      } //: null
 
-    return(
-      <defs>
-        <linearGradient id="Gradient1" {...directions}>
-          <stop offset="0%" stopColor={`rgb(${color},0)`}/>
-          <stop offset="100%" stopColor={`rgb(${color})`}/>
-        </linearGradient>
-      </defs>
-    )
-  }
+  //   return(
+  //     <defs>
+  //       <linearGradient id="Gradient1" {...directions}>
+  //         <stop offset="0%" stopColor={`rgb(${color},0)`}/>
+  //         <stop offset="100%" stopColor={`rgb(${color})`}/>
+  //       </linearGradient>
+  //     </defs>
+  //   )
+  // }
 
   return (
     <React.Fragment>
-      <ColorGradientDefs color={`100, 149, 237`} direction={'down'}/>
-      {data && data.map( (data, index) =>
-        <g key={`${data.x}${data.index}`} className="singleBar">
-          <rect
-            key={data.x}
-            x={xScale(data.x)}
-            y={yScale(data.y)}
-            height={height - margins.bottom - scales.yScale(data.y)}
-            width={xScale.bandwidth()}
-            fill={`url(#Gradient1)`} //colorScale(index)
-            className={'singleRect'}
-          />
-          <text className={"barLabel"}
-            x={xScale(data.x) + (.3 * xScale.bandwidth()) }
-            y={yScale(data.y) * .99}
-          >
-            {(data.y)}
-          </text>
-        </g>,
-      )}
+      {data && data.map( (data, index) => {
+        return(<React.Fragment key={data.x}>
+          <defs>
+            <linearGradient id={`Gradient${index}`} {...directions}>
+              <stop offset="0%" stopColor={`rgb(0,0,0,.01)`}/>
+              <stop offset="100%" stopColor={`${colorScale(index)}`}/>
+            </linearGradient>
+          </defs>
+          <g className="singleBar">
+            <rect
+              x={xScale(data.x)}
+              y={yScale(data.y)}
+              height={height - margins.bottom - scales.yScale(data.y)}
+              width={xScale.bandwidth()}
+              fill={`url(#Gradient${index})`} //colorScale(index)
+              className={'singleRect'}
+            />
+            <text className={"barLabel"}
+              x={xScale(data.x) + (.3 * xScale.bandwidth()) }
+              y={yScale(data.y) * .99}
+            >
+              {(data.y)}
+            </text>
+          </g>
+        </React.Fragment>)
+
+      })}
     </React.Fragment>
   )
 }
