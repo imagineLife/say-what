f5e6afca3b79ef689b7aacfba694b0d1a15fce82
Eric Laursen <ericjlaursen@gmail.com>
2019-06-13 07:02:56 -0400

    Includes gradient in bar coloring

--- src/components/d3/BarChart/Bars/index.js
+++ src/components/d3/BarChart/Bars/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { scaleLinear } from 'd3-scale'
 import { interpolateLab } from 'd3-interpolate'
-import './Bars.css'
+import './index.css'
 
 export default function Bars({ scales, margins, data, svgDimensions }){
 
@@ -13,8 +13,19 @@ export default function Bars({ scales, margins, data, svgDimensions }){
     .interpolate(interpolateLab)
     .domain([0, data.length - 1]);
 
+  let ColorGradientDefs = () => {
+    return(
+      <defs>
+        <linearGradient id="Gradient1" x1="1" x2="1" y1="1" y2="0">
+          <stop offset="0%" stopColor="rgba(0,0,255,0)"/>
+          <stop offset="100%" stopColor="blue"/>
+        </linearGradient>
+      </defs>
+    )
+  }
   return (
     <React.Fragment>
+      <ColorGradientDefs />
       {data && data.map( (data, index) =>
         <g key={`${data.x}${data.index}`} className="singleBar">
           <rect
@@ -23,9 +34,10 @@ export default function Bars({ scales, margins, data, svgDimensions }){
             y={yScale(data.y)}
             height={height - margins.bottom - scales.yScale(data.y)}
             width={xScale.bandwidth()}
-            fill={colorScale(index)}
+            fill={`url(#Gradient1)`}
+            className={'singleRect'}
           />
-          <text className="barLabel"
+          <text className={"barLabel"}
             x={xScale(data.x) + (.3 * xScale.bandwidth()) }
             y={yScale(data.y) * .99}
           >
