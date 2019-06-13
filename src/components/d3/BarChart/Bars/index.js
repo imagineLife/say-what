1691f32e2d256b473819abf7c2b0cb0261f54a9d
Eric Laursen <ericjlaursen@gmail.com>
2019-06-12 22:10:33 -0400

    Converts to fn component

--- src/components/d3/BarChart/Bars/index.js
+++ src/components/d3/BarChart/Bars/index.js
@@ -3,45 +3,40 @@ import { scaleLinear } from 'd3-scale'
 import { interpolateLab } from 'd3-interpolate'
 import './Bars.css'
 
-export default class Bars extends Component {
-  constructor(props) {
-    super(props)
+export default function Bars(props){
 
-    this.colorScale = scaleLinear()
-      .range(['#256CFF','#143B8A'])
-      .interpolate(interpolateLab)
-  }
+  const colorScale = scaleLinear()
+    .range(['#256CFF','#143B8A'])
+    .interpolate(interpolateLab)
 
-  render() {
-    const { scales, margins, data, svgDimensions } = this.props
-    const { xScale, yScale } = scales
-    const { height } = svgDimensions
-    
-    this.colorScale.domain([0, data.length - 1]);
+  const { scales, margins, data, svgDimensions } = props
+  const { xScale, yScale } = scales
+  const { height } = svgDimensions
+  
+  colorScale.domain([0, data.length - 1]);
 
-    const bars = (
-      data.map( (data, index) =>
-        <g key={data.word} className="singleBar">
-          <rect
-            key={data.word}
-            x={xScale(data.word)}
-            y={yScale(data.occurances)}
-            height={height - margins.bottom - scales.yScale(data.occurances)}
-            width={xScale.bandwidth()}
-            fill={this.colorScale(index)}
-          />
-          <text className="barLabel"
-            x={xScale(data.word) + (.3 * xScale.bandwidth()) }
-            y={yScale(data.occurances) * .99}
-          >
-            {(data.occurances)}
-          </text>
-        </g>,
-      )
+  const bars = (
+    data.map( (data, index) =>
+      <g key={data.word} className="singleBar">
+        <rect
+          key={data.word}
+          x={xScale(data.word)}
+          y={yScale(data.occurances)}
+          height={height - margins.bottom - scales.yScale(data.occurances)}
+          width={xScale.bandwidth()}
+          fill={colorScale(index)}
+        />
+        <text className="barLabel"
+          x={xScale(data.word) + (.3 * xScale.bandwidth()) }
+          y={yScale(data.occurances) * .99}
+        >
+          {(data.occurances)}
+        </text>
+      </g>,
     )
+  )
 
-    return (
-      <g className="barWrapper">{bars}</g>
-    )
-  }
+  return (
+    <g className="barWrapper">{bars}</g>
+  )
 }
