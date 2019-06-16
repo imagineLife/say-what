25446c1ef441e03dc9baae153fe4bbcd4d61ffe4
Eric Laursen <ericjlaursen@gmail.com>
2019-06-16 13:06:31 -0400

    converts to Fn

--- src/components/d3/bubbleChart/Chart/index.js
+++ src/components/d3/bubbleChart/Chart/index.js
@@ -6,82 +6,65 @@ import ResponsiveWrapper from '../../../ResponsiveWrapper'
 import './Chart.css'
 
 
-class Chart extends Component {
-  constructor(props) {
-    super(props);
-    // this.enterBubbleFn = this.enterBubbleFn.bind(this);
-    // this.updateBubbleFn = this.updateBubbleFn.bind(this)
-    this.color = d3.scaleOrdinal(d3.schemeCategory10);
-    this.format = d3.format(',d');
-  }
-
-  componentDidMount(){
-    // this.buildChart();
-  }
-
-  componentDidUpdate(){
-    // this.buildChart();
-  }
-
-  render() {
-    
-    let sizeToUse = this.props.respWrapWidth * .85
-    //PACK
-    var d3PackFn = d3.pack()
-        .size([sizeToUse, sizeToUse]);
+function Chart(props) {
+  let color = d3.scaleOrdinal(d3.schemeCategory10);
+   
+  let sizeToUse = props.respWrapWidth * .85
+  //PACK
+  var d3PackFn = d3.pack()
+      .size([sizeToUse, sizeToUse]);
 
 
-    //declare ROOT
-    var root = d3.hierarchy({children: this.props.dataKey})
-      .sum(d => d.occurances);
+  //declare ROOT
+  var root = d3.hierarchy({children: props.data})
+    .sum(d => d.occurances);
 
-    let packedCircleRoot = d3PackFn(root);
+  let packedCircleRoot = d3PackFn(root);
 
-    let packedCircles = packedCircleRoot.leaves()
+  let packedCircles = packedCircleRoot.leaves()
 
 
-    return (
-      <svg className='bubbleSVGWrapper' viewBox={`25, 0, ${sizeToUse}, ${sizeToUse}`}>
-        {packedCircles && packedCircles.map((c, ind) => {
-        return (<g 
-          key={`circle${ind}`} 
-          className="singleBubbleG"
-          transform={`translate(${c.x},${c.y})`}>
-            <clipPath xlinkHref={`#clip-${c.data.size.toString()}`}></clipPath>
-            <circle 
-              r={c.r}
-              fill={this.color(ind)}>
-            </circle>
-            <text 
-              className="clipText" 
-              clipPath={`url(#clip-${c.data.size.toString()})`}>
-              <tspan 
-                className="bubbleText title"
-                textAnchor={"middle"}
-                x={0}
-                y={-5}>
-                {`${c.data.size}-Letter`}
-              </tspan>
-              <tspan 
-                className="bubbleText title"
-                x={0}
-                y={11}
-                textAnchor={"middle"}>
-                Words
-              </tspan>
-              <tspan 
-                className="bubbleText val"
-                x={0}
-                y={30}
-                textAnchor={"middle"}>
-                {`${c.data.occurances}`}
-              </tspan>
-            </text>
-          </g>)
-    })}
-      </svg>
-    )
-  }
+  return (
+    <svg className='bubbleSVGWrapper' viewBox={`25, 0, ${sizeToUse}, ${sizeToUse}`}>
+      {packedCircles && packedCircles.map((c, ind) => {
+      return (<g 
+        key={`circle${ind}`} 
+        className="singleBubbleG"
+        transform={`translate(${c.x},${c.y})`}>
+          <clipPath xlinkHref={`#clip-${c.data.size.toString()}`}></clipPath>
+          <circle 
+            r={c.r}
+            fill={color(ind)}>
+          </circle>
+          <text 
+            className="clipText" 
+            clipPath={`url(#clip-${c.data.size.toString()})`}>
+            <tspan 
+              className="bubbleText title"
+              textAnchor={"middle"}
+              x={0}
+              y={-5}>
+              {`${c.data.size}-Letter`}
+            </tspan>
+            <tspan 
+              className="bubbleText title"
+              x={0}
+              y={11}
+              textAnchor={"middle"}>
+              Words
+            </tspan>
+            <tspan 
+              className="bubbleText val"
+              x={0}
+              y={30}
+              textAnchor={"middle"}>
+              {`${c.data.occurances}`}
+            </tspan>
+          </text>
+        </g>)
+      })}
+    </svg>
+  )
 }
 
 export default ResponsiveWrapper(Chart)
