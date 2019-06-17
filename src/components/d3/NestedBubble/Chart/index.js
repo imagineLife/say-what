65b9371d5554cb10ae0bc8760c39435e35993f40
Eric Laursen <ericjlaursen@gmail.com>
2019-06-16 20:06:23 -0400

    initial nested circles

--- src/components/d3/NestedBubble/Chart/index.js
+++ src/components/d3/NestedBubble/Chart/index.js
@@ -6,82 +6,78 @@ import ResponsiveWrapper from '../../../ResponsiveWrapper'
 import './Chart.css'
 
 
-class Chart extends Component {
-  constructor(props) {
-    super(props);
-    // this.enterBubbleFn = this.enterBubbleFn.bind(this);
-    // this.updateBubbleFn = this.updateBubbleFn.bind(this)
-    this.color = d3.scaleOrdinal(d3.schemeCategory10);
-    this.format = d3.format(',d');
+function Chart({respWrapWidth, data, radiusKey, categoryKey}) {
+  
+  let color = d3.scaleOrdinal(d3.schemeCategory10);
+  
+  let sizeToUse = respWrapWidth
+  //PACK
+  var d3PackFn = d3.pack()
+      .size([sizeToUse, sizeToUse])
+      .padding(3);
+  
+  let packedCircles = d3PackFn(data)
+  
+  let allCircles = []
+
+  let circleObj = (d) => {
+    return {
+      r : d.r,
+      y : d.y,
+      x : d.x,
+      value : d.value,
+      id: d.data.id,
+      text: (d.data.id !== 'wordCount') ? `${d.data.id} Words` : null
+    }
   }
 
-  componentDidMount(){
-    // this.buildChart();
+  allCircles.push(circleObj(packedCircles))
+  if(packedCircles && packedCircles.children){
+    packedCircles.children.forEach(circle => {
+      console.log('circle')
+      console.log(circle)
+      
+      let thisCircle = circleObj(circle)
+      allCircles.push(thisCircle)
+    })
   }
 
-  componentDidUpdate(){
-    // this.buildChart();
-  }
-
-  render() {
-    
-    let sizeToUse = this.props.respWrapWidth * .85
-    //PACK
-    var d3PackFn = d3.pack()
-        .size([sizeToUse, sizeToUse]);
-
-
-    //declare ROOT
-    var root = d3.hierarchy({children: this.props.dataKey})
-      .sum(d => d.occurances);
-
-    let packedCircleRoot = d3PackFn(root);
-
-    let packedCircles = packedCircleRoot.leaves()
-
-
-    return (
-      <svg className='bubbleSVGWrapper' viewBox={`25, 0, ${sizeToUse}, ${sizeToUse}`}>
-        {packedCircles && packedCircles.map((c, ind) => {
+  return (
+    <svg className='bubbleSVGWrapper' viewBox={`25, 0, ${sizeToUse}, ${sizeToUse}`}>
+      <g className="gWrapper" transform={`translate(${25},0)`}>
+        {allCircles && allCircles.map((c, ind) => {
         return (<g 
           key={`circle${ind}`} 
           className="singleBubbleG"
           transform={`translate(${c.x},${c.y})`}>
-            <clipPath xlinkHref={`#clip-${c.data.size.toString()}`}></clipPath>
+            <clipPath xlinkHref={`#clip-${c[categoryKey].toString()}`}></clipPath>
             <circle 
               r={c.r}
-              fill={this.color(ind)}>
+              fill={color(ind)}>
             </circle>
             <text 
               className="clipText" 
-              clipPath={`url(#clip-${c.data.size.toString()})`}>
+              clipPath={`url(#clip-${c[categoryKey].toString()})`}>
               <tspan 
                 className="bubbleText title"
                 textAnchor={"middle"}
                 x={0}
                 y={-5}>
-                {`${c.data.size}-Letter`}
-              </tspan>
-              <tspan 
-                className="bubbleText title"
-                x={0}
-                y={11}
-                textAnchor={"middle"}>
-                Words
+                {c.text}
               </tspan>
               <tspan 
                 className="bubbleText val"
                 x={0}
                 y={30}
                 textAnchor={"middle"}>
-                {`${c.data.occurances}`}
+                {c.value}
               </tspan>
             </text>
           </g>)
-    })}
-      </svg>
-    )
-  }
+        })}
+      </g>
+    </svg>
+  )
 }
 
 export default ResponsiveWrapper(Chart)
