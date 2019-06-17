87de8919c1c9c8de1275c245d5ac43d93988a62d
Eric Laursen <ericjlaursen@gmail.com>
2019-06-16 20:18:54 -0400

    Introduces subCircle nested elementes

--- src/components/d3/NestedBubble/Chart/index.js
+++ src/components/d3/NestedBubble/Chart/index.js
@@ -8,6 +8,12 @@ import './Chart.css'
 
 function Chart({respWrapWidth, data, radiusKey, categoryKey}) {
   
+  let mouseOver = (circleData) => {
+    console.log('circleData')
+    console.log(circleData)
+    
+  }
+
   let color = d3.scaleOrdinal(d3.schemeCategory10);
   
   let sizeToUse = respWrapWidth
@@ -27,11 +33,13 @@ function Chart({respWrapWidth, data, radiusKey, categoryKey}) {
       x : d.x,
       value : d.value,
       id: d.data.id,
-      text: (d.data.id !== 'wordCount') ? `${d.data.id} Words` : null
+      text: (d.data.id !== 'wordCount') ? `${d.data.id} Words` : null,
+      root: (d.data.id !== 'wordCount') ? true : false
     }
   }
 
   allCircles.push(circleObj(packedCircles))
+  
   if(packedCircles && packedCircles.children){
     packedCircles.children.forEach(circle => {
       console.log('circle')
@@ -39,6 +47,13 @@ function Chart({respWrapWidth, data, radiusKey, categoryKey}) {
       
       let thisCircle = circleObj(circle)
       allCircles.push(thisCircle)
+
+      if(circle && circle.children){
+        circle.children.forEach(subCircle => {
+          let thisSubCircle = circleObj(subCircle)
+          allCircles.push(thisSubCircle)
+        })
+      }
     })
   }
 
@@ -46,35 +61,39 @@ function Chart({respWrapWidth, data, radiusKey, categoryKey}) {
     <svg className='bubbleSVGWrapper' viewBox={`25, 0, ${sizeToUse}, ${sizeToUse}`}>
       <g className="gWrapper" transform={`translate(${25},0)`}>
         {allCircles && allCircles.map((c, ind) => {
-        return (<g 
-          key={`circle${ind}`} 
-          className="singleBubbleG"
-          transform={`translate(${c.x},${c.y})`}>
-            <clipPath xlinkHref={`#clip-${c[categoryKey].toString()}`}></clipPath>
-            <circle 
-              r={c.r}
-              fill={color(ind)}>
-            </circle>
-            <text 
-              className="clipText" 
-              clipPath={`url(#clip-${c[categoryKey].toString()})`}>
-              <tspan 
-                className="bubbleText title"
-                textAnchor={"middle"}
-                x={0}
-                y={-5}>
-                {c.text}
-              </tspan>
-              <tspan 
-                className="bubbleText val"
-                x={0}
-                y={30}
-                textAnchor={"middle"}>
-                {c.value}
-              </tspan>
-            </text>
-          </g>)
-        })}
+          if(c.root == true){
+            return (<g 
+              key={`circle${ind}`} 
+              className="singleBubbleG"
+              transform={`translate(${c.x},${c.y})`}>
+                <clipPath xlinkHref={`#clip-${c[categoryKey].toString()}`}></clipPath>
+                <circle 
+                  r={c.r}
+                  fill={color(ind)}
+                  cursor='pointer'
+                  onMouseOver={(() => mouseOver(c))}>
+                </circle>
+                <text 
+                  className="clipText" 
+                  clipPath={`url(#clip-${c[categoryKey].toString()})`}>
+                  <tspan 
+                    className="bubbleText title"
+                    textAnchor={"middle"}
+                    x={0}
+                    y={-5}>
+                    {c.text}
+                  </tspan>
+                  <tspan 
+                    className="bubbleText val"
+                    x={0}
+                    y={30}
+                    textAnchor={"middle"}>
+                    {c.value}
+                  </tspan>
+                </text>
+              </g>)
+          }else{null}
+        }).filter(d => d)}
       </g>
     </svg>
   )
