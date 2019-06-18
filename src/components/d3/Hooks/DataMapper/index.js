928974ddefa9fb747df664201226555c5efa31de
Eric Laursen <ericjlaursen@gmail.com>
2019-06-18 13:40:40 -0400

    moves dataMapping to Hook

---
+++ src/components/d3/Hooks/DataMapper/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//set x && y keys to a re-mapped data object
+function remapData(srcData, xVal, yVal){
+  let [resData, setResData] = React.useState(null)
+  React.useEffect(() => {
+    setResData(srcData.map((d,ind) => {
+      return {
+        x: (xVal == 'index') ? ind : d[xVal],
+        y: d[yVal],
+      }
+    }))
+  }, [])
+  return resData
+}
+
+export default remapData
