82dfd88b682551e713854852d92579a96cdf512d
Eric Laursen <ericjlaursen@gmail.com>
2019-06-29 20:43:34 -0400

    updates comments

--- src/components/d3/Hooks/DataMapper/index.js
+++ src/components/d3/Hooks/DataMapper/index.js
@@ -1,12 +1,14 @@
-/*
-  Maps data to {x, y} type res object
-*/
-
 import React from 'react';
 
-//set x && y keys to a re-mapped data object
+/*
+  DATA MAPPER
+  Maps data to {x, y} format object
+*/
+
 function remapData(srcData, xVal, yVal){
+  
   let [resData, setResData] = React.useState(null)
+
   React.useEffect(() => {
     setResData(srcData.map((d,ind) => {
       return {
