9f4b06db2d0d6cefd8d98178b3796932227a42de
Eric Laursen <ericjlaursen@gmail.com>
2019-07-04 11:54:12 -0400

    title && most-used-words section

--- src/containers/WireFrame/index.js
+++ src/containers/WireFrame/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const WireFrame = () => {
-	return (<div id="wireFrame">
-		<p>WireFrame Here</p>
-	</div>)
+	return (<React.Fragment>
+	  <main id="wireFrame">
+		<section id="compare-picker">
+			<h2>Compare Statistics</h2>
+		</section>
+		<section id="common-words">
+			<h2>Most-Used Words</h2>
+			<p>Review the words that are most used across speeches.**</p>
+		</section>
+	  </main>
+	  <footer>
+		<p>** - The <a href="https://en.wikipedia.org/wiki/Most_common_words_in_English">30 Most Common words</a> in the english language have been omitted.</p>
+	  </footer>
+	</React.Fragment>)
 }
 
 export default WireFrame
