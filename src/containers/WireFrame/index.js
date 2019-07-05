9156a553f70e3c2830785cfcb93b690e73064f56
Eric Laursen <ericjlaursen@gmail.com>
2019-07-04 20:51:35 -0400

    Updates placeholder text

--- src/containers/WireFrame/index.js
+++ src/containers/WireFrame/index.js
@@ -8,11 +8,16 @@ const WireFrame = () => {
 		</section>
 		<section id="common-words">
 			<h2>Most-Used Words</h2>
-			<p>Review the words that are most used across speeches.**</p>
+			<p>Across Party lines &&  throughout time, the most used words thoughout these texts.**</p>
+		</section>
+		<section id="longest-sentences">
+			<h2>Longest Sentences</h2>
+			<p><a>Several Studies ***</a> have dug in to the details meaning of sentence structure, length etc.</p>
 		</section>
 	  </main>
 	  <footer>
 		<p>** - The <a href="https://en.wikipedia.org/wiki/Most_common_words_in_English">30 Most Common words</a> in the english language have been omitted.</p>
+	  	<p>*** - <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1761115/">A study focusing on children with specific language impairment</a></p>
 	  </footer>
 	</React.Fragment>)
 }
