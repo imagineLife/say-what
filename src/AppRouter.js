ca7b358939fe3f2bde9ab22e905541d4bd236da4
Eric Laursen <ericjlaursen@gmail.com>
2019-07-05 20:39:00 -0400

    Render Props of WireFrame && passes speech array

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -16,6 +16,37 @@ const Comparison = lazy(() => import('./containers/Comparison'))
 const WireFrame = lazy(() => import('./containers/WireFrame'))
 import Login from './containers/Login';
 
+let speeches = [
+	{
+		title: "speech one",
+		id: "1234"
+	},
+	{
+		title: "speech two",
+		id: "2345"
+	},
+	{
+		title: "speech three",
+		id: "3456"
+	},
+	{
+		title: "speech four",
+		id: "4567"
+	},
+	{
+		title: "speech five",
+		id: "5678"
+	},
+	{
+		title: "speech six",
+		id: "6789"
+	},
+	{
+		title: "speech seven",
+		id: "7890"
+	}
+]
+
 function AppRouter(){
   return (
 	<Router>
@@ -27,7 +58,9 @@ function AppRouter(){
 		        <Route exact path="/speechText/:id" component={SpeechText} />
 		        <Route exact path="/speechPicker" component={SpeechPicker} />
 		        <Route exact path="/compare" component={Comparison} />
-		        <Route exact path="/wireframe" component={WireFrame} />
+		        <Route exact path="/wireframe" render={() => {
+		        	return(<WireFrame speeches={speeches} />)
+		        }} />
 		        <Route exact path="/login" component={Login} />
 		        <Redirect from='/*' to="/speechData/default" />
 		    </Switch>
