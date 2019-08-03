faaddd8e9aba928aded0aeed6ecce5c950b1cef7
Eric Laursen <ericjlaursen@gmail.com>
2019-08-03 16:02:26 -0400

    includes shorthand props

--- src/components/Forms/SpeechText/index.js
+++ src/components/Forms/SpeechText/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import '../Forms.css';
 import {Link} from 'react-router-dom';
 
-export default function SpeechTextForm(props) {
+export default function SpeechTextForm({speechID}: {speechID: string}) {
   return (
 		<form className='begin-form'>
       <div>
         <label htmlFor="speechText">Read through the entire speech</label>
-          <Link to={`/speechText/${props.speechID}`}>
+          <Link to={`/speechText/${speechID}`}>
             <button type='button' id="speechText">
               Here!
             </button>
