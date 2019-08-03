a198b89829ee88fb17d13b5575c68c089c726302
Eric Laursen <ericjlaursen@gmail.com>
2019-08-03 15:51:46 -0400

    TempStateType

--- src/components/Forms/Register/flow.js
+++ src/components/Forms/Register/flow.js
@@ -2,3 +2,11 @@ export type propsType = {
   toggleForm: () => mixed,
   myRunLoginKey: () => mixed,
 }
+
+export type TempStateType = {
+  firstName: string,
+  lastName: string,
+  username: string,
+  password: string,
+  email: string
+}
