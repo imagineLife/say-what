2bea7f05695d5ec5dafae0ee903fc307ecf8f01b
Eric Laursen <ericjlaursen@gmail.com>
2019-08-11 14:07:15 -0400

    makes props optional, to pass test

--- src/components/Section/flow.js
+++ src/components/Section/flow.js
@@ -3,15 +3,15 @@ export type SpeechItemType = {
 }
 
 export type PropsTypes = {
-	title: string, 
-	img: () => void, 
-	text: string, 
-	speechPicker: boolean, 
-	speechesFromAPI: Array<SpeechItemType>, 
-	form: boolean, 
-	includeBeginForm: boolean,
-	includeRequestForm: boolean,
-	requested: boolean,
-	includeLogoutForm: boolean,
-	includeBottomSpace: boolean 
+	title?: string, 
+	img?: () => void, 
+	text?: string, 
+	speechPicker?: boolean, 
+	speechesFromAPI?: Array<SpeechItemType>, 
+	form?: boolean, 
+	includeBeginForm?: boolean,
+	includeRequestForm?: boolean,
+	requested?: boolean,
+	includeLogoutForm?: boolean,
+	includeBottomSpace?: boolean 
 }
