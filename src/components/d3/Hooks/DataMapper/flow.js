3b26dba2bb06aa855588763de2a8e24ab55c7599
Eric Laursen <ericjlaursen@gmail.com>
2019-08-16 16:04:49 -0400

    updates propTypes

--- src/components/d3/Hooks/DataMapper/flow.js
+++ src/components/d3/Hooks/DataMapper/flow.js
@@ -2,3 +2,15 @@ export type SrcDataItem = {
 	text: string,
 	wordCount: number
 }
+
+export type ResObjType = {
+	x: number,
+	y: number
+}
+
+export type ResType = {
+  x: string | number,
+  y: number
+}
+
+export type dataType = any
--- src/components/d3/Hooks/DataMapper/index.js
+++ src/components/d3/Hooks/DataMapper/index.js
@@ -1,9 +1,10 @@
-import type { SrcDataItem } from './flow'
+import type { SrcDataItem, ResType, dataType } from './flow'
 import React from 'react';
 
 /*
   DATA MAPPER
   Maps data to {x, y} format object
+  converts [{word: thisWord, count: 17}] to [{x: thisWord, y: 17}]
 */
 
 function remapData(srcData: Array<SrcDataItem>, xVal: string, yVal: string){
@@ -11,11 +12,16 @@ function remapData(srcData: Array<SrcDataItem>, xVal: string, yVal: string){
   let [resData, setResData] = React.useState(null)
 
   React.useEffect(() => {
-    setResData(srcData.map((d,ind) => {
-      return {
+    // setResData(srcData.map((d,ind) => {
+    setResData(srcData.map((d: dataType, ind: number) => {
+
+      const res: ResType = {
         x: (xVal == 'index') ? ind : d[xVal],
         y: d[yVal],
       }
+
+      return res;
+
     }))
   }, [])
   return resData
