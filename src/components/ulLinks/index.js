4a35a46eb552fdd684ad11de2b2dceb5ef4ba78f
Eric Laursen <ericjlaursen@gmail.com>
2019-08-17 07:40:14 -0400

    Imports types

--- src/components/ulLinks/index.js
+++ src/components/ulLinks/index.js
@@ -1,16 +1,17 @@
+import type { SpeechPartsType } from './flow';
 import React from 'react';
 import './ulLinks.css';
 import SpeechPickerLi from '../SpeechPickerLi';
 import {Link} from 'react-router-dom'
 
-export default function UlLink(props) {
+export default function UlLink({speechParts}: SpeechPartsType) {
      let parseDateToYear = (dateToParse) => {
 	    let usableDate = new Date(dateToParse);
 		let year = (usableDate.getFullYear() < 10 ? ('0'+usableDate.getFullYear()) : usableDate.getFullYear());
 		return year;	
      }
 
-	const speechLinks = props.speechParts.map((speech, ind) =>
+	const speechLinks = speechParts.map((speech, ind) =>
 
 		<Link className="ulLink" key={ind} to={`speechData/${speech._id}`} >
 			<SpeechPickerLi word={`${parseDateToYear(speech.Date)} ${speech.title}`} boldWord={speech.Orator} />
