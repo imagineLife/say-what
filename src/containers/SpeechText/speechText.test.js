dd96699e99adec2519b63f0f51b7ab5fc16243e0
Eric Laursen <ericjlaursen@gmail.com>
2019-08-23 16:57:46 -0400

    includes 2 tests

--- src/containers/SpeechText/speechText.test.js
+++ src/containers/SpeechText/speechText.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {SpeechText} from './index';
+import { SpeechText } from './index';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 require('isomorphic-fetch')
 
 describe('SpeechText.js', () => {
 	it('renders without crashing', () => {
-	  shallow(<SpeechText />);
+	  const comp = shallow(<SpeechText />);
+	  expect(comp.length).toBe(1)
 	});
 
-	// it('Renders 1 <Section />s', () => {
-	// 	const test = shallow(<SpeechText />);
-	// 	const instance = test.instance();
-	// 	const mockSection = test.find('Section');
-	// 	expect(mockSection.length).toEqual(1);
-	// })
+	it('Renders 1 <Section />s', () => {
+		const test = shallow(<SpeechText />);
+		const instance = test.instance();
+		const mockSection = test.find('Section');
+		expect(mockSection.length).toEqual(1);
+	})
 })
