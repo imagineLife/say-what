60c54fffbc2391d68d620cf8505111391fc57350
Eric Laursen <ericjlaursen@gmail.com>
2019-08-23 05:57:34 -0400

    includes a localStorage mock

--- src/containers/SpeechPicker/speechPicker.test.js
+++ src/containers/SpeechPicker/speechPicker.test.js
@@ -7,17 +7,40 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('SpeechPicker.js', () => {
-	window.backendPath = '//localhost:8080';
-	it('renders without crashing', () => {
-	  const test = shallow(<SpeechPicker />);
-	  expect(test.length).toEqual(1);
+
+	var localStorageMock = (function() {
+	    var store = {};
+
+	    return {
+	        getItem: function(key) {
+	            return store[key] || null;
+	        },
+	        setItem: function(key, value) {
+	            store[key] = value.toString();
+	        },
+	        clear: function() {
+	            store = {};
+	        }
+	    };
+
+	})();
+
+	Object.defineProperty(window, 'localStorage', {
+	     value: localStorageMock
 	});
 
+	window.backendPath = '//localhost:8080';
 	it('Renders <Redirect/> when not-logged-in', () => {
-		const test = shallow(<SpeechPicker />);
-		const instance = test.instance();
-		const mockSection = test.find('Redirect');
-		expect(mockSection.length).toEqual(1);
+	  const test = shallow(<SpeechPicker />);
+	  const mockSection = test.find('Redirect');
+	  expect(mockSection.length).toEqual(1);
+	});
+
+	it.only('uses the localStorage auth token to try to login', () => {
+		localStorage.setItem('localStorageAuthToken', 'dummyAuthToken')
+		expect(localStorage.getItem('localStorageAuthToken')).toEqual('dummyAuthToken');
 	})
+
+
 })
 
