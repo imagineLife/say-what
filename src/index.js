2667c51069c053cbe6f96c86a2b1b9230ee47911
Eric Laursen <ericjlaursen@gmail.com>
2020-10-18 14:40:36 -0400

    import App

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './AppRouter';
-import {Provider} from 'react-redux';
-import myConfigureStore from './store';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import myConfigureStore from "./store";
+import App from "./App";
+
+import "./index.css";
 
 //copied from serviceWorker
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+  window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    window.location.hostname === "[::1]" ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
 
-window.backendPath = isLocalhost ? '//localhost:8080' : '//saywhat-speech-api.herokuapp.com';
+window.backendPath = isLocalhost
+  ? "//localhost:8080"
+  : "//saywhat-speech-api.herokuapp.com";
 
 const initialState = {};
 
 const store = myConfigureStore(initialState);
 
-ReactDOM.render(<Provider store={store}>
-		<AppRouter />
-	</Provider>
-		, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
