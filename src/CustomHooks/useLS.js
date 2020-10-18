c9ecc4aecbb545bd1f2eb7126b1b2d6aa5a335cc
Eric Laursen <ericjlaursen@gmail.com>
2020-10-18 15:30:11 -0400

    removeLogs

--- src/CustomHooks/useLS.js
+++ src/CustomHooks/useLS.js
@@ -1,34 +1,30 @@
-import React from 'react';
+import React from "react";
 
 //a CUSTOM HOOK!
 const useLocalStorage = (localStorageKey: string) => {
-
   //NOTE: includes a wrapper fn to work with useState,
   // invoked ONCE  to get initial value. This changes localStorage performance for the better
   const initialValue = () => {
-    let lsVal = window.localStorage.getItem(localStorageKey)
-    console.log('lsVal')
-    console.log(lsVal)
-    
+    let lsVal = window.localStorage.getItem(localStorageKey);
     //GET item from localStorage
     return lsVal;
-  }
+  };
 
   //get/set for EXISTING todos
   //tried initialValue: () => ?string)
-  const [storageval, setStorageval] = React.useState(initialValue)
+  const [storageval, setStorageval] = React.useState(initialValue);
 
   /*
     WRITE val to localstorage
     NOTE: arr @ end means ONLY run if 'todos' has changed
   */
   React.useEffect(() => {
-    if(storageval !== null){
+    if (storageval !== null) {
       window.localStorage.setItem(localStorageKey, JSON.stringify(storageval));
     }
-  }, [storageval])
+  }, [storageval]);
 
-  return [storageval, setStorageval]
-}
+  return [storageval, setStorageval];
+};
 
-export default useLocalStorage
+export default useLocalStorage;
