203315cea5c746d678c5d83f62fe77ae5fba95c3
Eric Laursen <ericjlaursen@gmail.com>
2020-10-19 20:03:27 -0400

    lint + destructured react imports

--- src/containers/SpeechText/index.js
+++ src/containers/SpeechText/index.js
@@ -1,120 +1,111 @@
-import React from 'react';
-import './SpeechText.css';
-import Section from '../../components/Section';
-import {getSpeechTextAxn} from './state/actions';
-import {connect} from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./SpeechText.css";
+import Section from "../../components/Section";
+import { getSpeechTextAxn } from "./state/actions";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-const SpeechText = (props) => {
-	let [error, setError] = React.useState(null);
-	let [loading, setLoading] = React.useState(false);
-	let [speechText, setSpeechText] = React.useState(null);
-	let [speechTitle, setSpeechTitle] = React.useState(null);
-	let [redirect, setRedirect] = React.useState(false)
+const SpeechText = props => {
+  let [error, setError] = useState(null);
+  let [loading, setLoading] = useState(false);
+  let [speechText, setSpeechText] = useState(null);
+  let [speechTitle, setSpeechTitle] = useState(null);
+  let [redirect, setRedirect] = useState(false);
 
-	React.useEffect(() => {
-		loadSpeechText();
-	}, [])
+  useEffect(() => {
+    loadSpeechText();
+  }, []);
 
-	const loadSpeechText = () => {
-		console.log('Loading speech text...');
-		setError(null),
-		setLoading(true)
-		// speechText : this.props.runSpeechTextAction(this.props.speechID)
+  const loadSpeechText = () => {
+    console.log("Loading speech text...");
+    setError(null), setLoading(true);
+    // speechText : this.props.runSpeechTextAction(this.props.speechID)
 
     //	Parsing the speechID from URL
-    
-        let fetchURL, urlText, lastPartOfURL;
-        urlText = window.location.href;
-        lastPartOfURL = urlText.split("/").pop();
 
-	// setting up the fetch url
-    
-    	lastPartOfURL === '5a1ad99f978ca2681f42df12' ? fetchURL = `${window.backendPath}/api/speeches/text/default` :fetchURL = `${window.backendPath}/api/speeches/text/${lastPartOfURL}`;
+    let fetchURL, urlText, lastPartOfURL;
+    urlText = window.location.href;
+    lastPartOfURL = urlText.split("/").pop();
+
+    // setting up the fetch url
+
+    lastPartOfURL === "5a1ad99f978ca2681f42df12"
+      ? (fetchURL = `${window.backendPath}/api/speeches/text/default`)
+      : (fetchURL = `${window.backendPath}/api/speeches/text/${lastPartOfURL}`);
 
     //	send & return speechText
     //	set speechText to container's state
 
-        return fetch(fetchURL, {
-		        method: 'GET',
-		        headers: {
-		            'Content-Type': 'application/json',
-		            'Authorization': 'Bearer ' + localStorage.getItem('localStorageAuthToken')
-		        }
-		    })
-            .then(res => {
-            	if(res.status == 401){
-            		console.log('redirecting...');
-            		setRedirect(true)
-            	}
-            	
-                if (!res.ok) {
-                    return Promise.reject(res.statusText);
-                }
-                return res.json();
-            })
-            .then(resText => {
-				setSpeechText(resText.text),
-				setSpeechTitle(resText.title),
-				setLoading(false)
-            })
-            .catch(err =>
-                setError('Could not load SpeechText'),
-                setLoading(false)
-            );
-	}
+    return fetch(fetchURL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("localStorageAuthToken")
+      }
+    })
+      .then(res => {
+        if (res.status == 401) {
+          console.log("redirecting...");
+          setRedirect(true);
+        }
 
-    
-    //WHEN loading...
-	if (loading) {
-    	return (
-			<main role="main" className="splashBack">
-		      <p>Processing Speech Text...</p>
-		    </main>
-    	);
-        
-	//WHEN not loading
-    } else {
+        if (!res.ok) {
+          return Promise.reject(res.statusText);
+        }
+        return res.json();
+      })
+      .then(({ text, title }) => {
+        setSpeechText(text), setSpeechTitle(title), setLoading(false);
+      })
+      .catch(err => setError("Could not load SpeechText"), setLoading(false));
+  };
 
-    	if(redirect === true){
-    		return <Redirect to="/login" />
-    	}
+  //WHEN loading...
+  if (loading) {
+    return (
+      <main role="main" className="splashBack">
+        <p>Processing Speech Text...</p>
+      </main>
+    );
 
-    	const sectionsArray =[
-			{
-				title: speechTitle,
-				sectionSpeechID: props.speechID,
-				// img: `[ Image of Orator behind Title ]`,
-				text: speechText,
-				includeBottomSpace:true
+    //WHEN not loading
+  } else {
+    if (redirect === true) {
+      return <Redirect to="/login" />;
+    }
 
-			}
-		];
+    const sectionsArray = [
+      {
+        title: speechTitle,
+        sectionSpeechID: props.speechID,
+        // img: `[ Image of Orator behind Title ]`,
+        text: speechText,
+        includeBottomSpace: true
+      }
+    ];
 
-		const sections = sectionsArray.map((sec,ind) => {
-	      	return <Section key={ind} {...sec}/>;
-		})
+    const sections = sectionsArray.map((sec, ind) => {
+      return <Section key={ind} {...sec} />;
+    });
 
-	    return (
-			<main role="main" className="splashBack">
-		      
-		      {sections}
+    return (
+      <main role="main" className="splashBack">
+        {sections}
+      </main>
+    );
+  }
+};
 
-		    </main>
-	    );
-	}
-}
+const mapDispatchToProps = dispatch => ({
+  runSpeechTextAction: speechId => {
+    getSpeechTextAxn(speechId, dispatch);
+  }
+});
 
-const mapDispatchToProps = (dispatch) =>
-  ({
-    runSpeechTextAction: (speechId) => { getSpeechTextAxn(speechId, dispatch); }
-  });
-
-const mapStateToProps = (state) =>
-({ 
-	speechID: state._root.entries["0"][1]
-})
+const mapStateToProps = state => ({
+  speechID: state._root.entries["0"][1]
+});
 
 //$FlowReduxBug
-export { SpeechText }
+export { SpeechText };
 export default connect(mapStateToProps, mapDispatchToProps)(SpeechText);
