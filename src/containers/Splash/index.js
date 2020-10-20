085c579612fd3fe0664e51db7c10c953d3c94032
Eric Laursen <ericjlaursen@gmail.com>
2020-10-19 20:00:20 -0400

    convert to fn component

--- src/containers/Splash/index.js
+++ src/containers/Splash/index.js
@@ -1,56 +1,47 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import './Splash.css';
-import HeaderSplash from '../../components/HeaderSplash';
-import Section from '../../components/Section';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Splash.css";
+import HeaderSplash from "../../components/HeaderSplash";
+import Section from "../../components/Section";
 
-export class Splash extends Component {
-		// constructor(props){
-		// 	super(props);
-		// }
+const Splash = props => {
+  const pageHeader = {
+    Title: `Say What?!`,
+    subTitle: `See data within speeches`
+  };
 
-		render(){
-			const pageHeader = {
-				Title: `Say What?!`,
-				subTitle: `See data within speeches`
-			}
+  const sectionsArray = [
+    {
+      title: `Notice What They're Saying`,
+      text: `SayWhat treats speeches like large datasets.`
+    },
+    {
+      title: `Pick from a List of Speeches`,
+      text: `Look at the words of Trump, Obama, Bill Clinton, & more!`
+    },
+    {
+      title: "Check It Out!",
+      includeBeginForm: true,
+      includeBottomSpace: true,
+      speechID: props.mappedSpeechID
+    }
+  ];
 
-			const sectionsArray =[
-				{
-					title: `Notice What They're Saying`,
-					text: `SayWhat treats speeches like large datasets.`
-				},
-				{
-					title: `Pick from a List of Speeches`,
-					text: `Look at the words of Trump, Obama, Bill Clinton, & more!`
-				},
-				{
-					title: 'Check It Out!',
-					includeBeginForm: true,
-					includeBottomSpace:true,
-					speechID: this.props.mappedSpeechID
-				}
-			];
-			
-			const sections = sectionsArray.map((sec,ind) => {
-		      	return <Section key={ind} {...sec}/>;
-			})
+  const sections = sectionsArray.map((sec, ind) => {
+    return <Section key={ind} {...sec} />;
+  });
 
+  return (
+    <main role="main" className="splashBack">
+      <HeaderSplash title={pageHeader.Title} subTitle={pageHeader.subTitle} />
+      <div className="row">{sections}</div>
+    </main>
+  );
+};
 
-		    return (
-				<main role="main" className="splashBack">
-				  <HeaderSplash title={pageHeader.Title} subTitle={pageHeader.subTitle} />
-			      <div className="row">
-			      	{sections} 
-			      </div>
-			    </main>
-		    );
-		}
-}
-
-const mapStateToProps = (state) => ({ 
-	mappedSpeechID: state._root.entries["0"][1]
-})
+const mapStateToProps = state => ({
+  mappedSpeechID: state._root.entries["0"][1]
+});
 
 //$FlowReduxBug
 export default connect(mapStateToProps)(Splash);
