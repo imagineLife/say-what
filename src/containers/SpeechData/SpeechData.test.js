5f9c1ebc276a2f0e845cd3ee729371a670ddf22d
Eric Laursen <ericjlaursen@gmail.com>
2020-10-20 05:49:24 -0400

    snapshot test

--- src/containers/SpeechData/SpeechData.test.js
+++ src/containers/SpeechData/SpeechData.test.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SpeechData } from './index';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-require('isomorphic-fetch')
+import React from "react";
+import ReactDOM from "react-dom";
+import SpeechData from "./SpeechData";
+import Enzyme, { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
+require("isomorphic-fetch");
 
 // Enzyme.configure({ adapter: new Adapter() });
 
-describe('SpeechData', () => {
-	
-	it('Calls CDM && MOCK loadStats', () => {
-	  jest.spyOn(SpeechData.prototype, 'componentDidMount')
-	  jest.spyOn(SpeechData.prototype, 'loadStats')
-      
-      const component = shallow(<SpeechData/>)
-      expect(SpeechData.prototype.componentDidMount.mock.calls.length).toBe(1)
-      expect(SpeechData.prototype.loadStats.mock.calls.length).toBe(1)
-	});
-})
+describe("<SpeechData />", () => {
+  it("matches snampshot", () => {
+    const component = shallow(<SpeechData />);
+    expect(toJson(component)).toMatchSnapshot();
+  });
+});
