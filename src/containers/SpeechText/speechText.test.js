c4fd06202e1d426778f78242bdf87d9c793eba3e
Eric Laursen <ericjlaursen@gmail.com>
2020-10-20 06:00:28 -0400

    just snapshot test

--- src/containers/SpeechText/speechText.test.js
+++ src/containers/SpeechText/speechText.test.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SpeechText } from './index';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-require('isomorphic-fetch')
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import SpeechText from "./SpeechText";
+import Enzyme, { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+require("isomorphic-fetch");
 
-describe('SpeechText.js', () => {
-	it('renders without crashing', () => {
-	  const comp = shallow(<SpeechText />);
-	  expect(comp.length).toBe(1)
-	});
-
-	it('Renders 1 <Section />s', () => {
-		const test = shallow(<SpeechText />);
-		const instance = test.instance();
-		const mockSection = test.find('Section');
-		expect(mockSection.length).toEqual(1);
-	})
-})
+describe("<SpeechText />", () => {
+  it("renders without crashing", () => {
+    const comp = shallow(<SpeechText />);
+    expect(comp.length).toBe(1);
+  });
+});
