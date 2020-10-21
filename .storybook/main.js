925814ccced9e74b08782321ca3358fe28f55cbb
Eric Laursen <ericjlaursen@gmail.com>
2020-10-21 06:24:32 -0400

    init

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: config => {
+    config.resolve.alias["core-js/modules"] = path.resolve(
+      __dirname,
+      "..",
+      "node_modules/@storybook/core/node_modules/core-js/modules"
+    );
+    return config;
+  }
+};
