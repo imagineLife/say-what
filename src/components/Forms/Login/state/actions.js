4141d47cee4b73f74c7564ac30b321c7c3f7248e
Eric Laursen <ericjlaursen@gmail.com>
2020-10-23 19:22:59 -0400

    lint + remove log

--- src/components/Forms/Login/state/actions.js
+++ src/components/Forms/Login/state/actions.js
@@ -1,34 +1,36 @@
-import type {loginActionObjType, dispatchObjectType} from '../flow'
-import {LOGIN} from './constants';
-export const loginAction = (obj: loginActionObjType, dispatch: (dispatchObj: dispatchObjectType) => void) => {
-    let encodedStr = btoa(`${obj.username}:${obj.password}`);
-    fetch(`${window.backendPath}/api/auth/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + encodedStr
-        },
-        body: JSON.stringify(obj)
-    })
+import type { loginActionObjType, dispatchObjectType } from "../flow";
+import { LOGIN } from "./constants";
+export const loginAction = (
+  obj: loginActionObjType,
+  dispatch: (dispatchObj: dispatchObjectType) => void
+) => {
+  let encodedStr = btoa(`${obj.username}:${obj.password}`);
+
+  fetch(`${window.backendPath}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Basic " + encodedStr
+    },
+    body: JSON.stringify(obj)
+  })
     .then(res => res.json())
-    .then(res => saveTokenAction(res,dispatch))
-    .catch(err => {
-        const {code} = err;
-        if (code === 401) {
-          console.log(code);
-        }
+    .then(res => {
+      saveTokenAction(res, dispatch);
     })
-}
+    .catch(err => {
+      const { code } = err;
+      if (code === 401) {
+        console.log(code);
+      }
+    });
+};
 
 const saveTokenAction = (obj, dispatch) => {
-
-    // saving to LOCAL storage in order to PERSIST across sessions
-    localStorage.setItem('localStorageAuthToken', obj.authToken);
-
-    // let dispatchObject: dispatchObjectType = {
-    let dispatchObject = {
-        type: LOGIN,
-        payload: obj
-    };
-    dispatch(dispatchObject);
-}
+  // saving to LOCAL storage in order to PERSIST across sessions
+  localStorage.setItem("localStorageAuthToken", obj.authToken);
+  dispatch({
+    type: LOGIN,
+    payload: obj
+  });
+};
